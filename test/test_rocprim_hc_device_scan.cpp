dcb4987ec73210c750b14c13d8455f24a5d3fdb4
Jakub Szuppe <jakub@streamhpc.com>
2018-01-29 16:45:00 +0100

    Tests for device_scan for HIP and HC API

---
+++ test/test_rocprim_hc_device_scan.cpp
@@ -0,0 +1,230 @@
+// MIT License
+//
+// Copyright (c) 2017 Advanced Micro Devices, Inc. All rights reserved.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+// Google Test
+#include <gtest/gtest.h>
+
+// HC API
+#include <hcc/hc.hpp>
+
+// rocPRIM HC API
+#include <device/device_scan_hc.hpp>
+
+#include "test_utils.hpp"
+
+namespace rp = rocprim;
+
+// Params for tests
+template<
+    class InputType,
+    class OutputType = InputType
+>
+struct DeviceScanParams
+{
+    using input_type = InputType;
+    using output_type = OutputType;
+};
+
+// ---------------------------------------------------------
+// Test for scan ops taking single input value
+// ---------------------------------------------------------
+
+template<class Params>
+class RocprimDeviceScanTests : public ::testing::Test
+{
+public:
+    using input_type = typename Params::input_type;
+    using output_type = typename Params::output_type;
+    const bool debug_synchronous = false;
+};
+
+typedef ::testing::Types<
+    // -----------------------------------------------------------------------
+    //
+    // -----------------------------------------------------------------------
+    DeviceScanParams<int>
+> RocprimDeviceScanTestsParams;
+
+std::vector<size_t> get_sizes()
+{
+    std::vector<size_t> sizes = {
+        2, 32, 65, 378,
+        1512, 3048, 4096,
+        27845, (1 << 16) + 1111
+    };
+    const std::vector<size_t> random_sizes = get_random_data<size_t>(2, 1, 16384);
+    sizes.insert(sizes.end(), random_sizes.begin(), random_sizes.end());
+    std::sort(sizes.begin(), sizes.end());
+    return sizes;
+}
+
+TYPED_TEST_CASE(RocprimDeviceScanTests, RocprimDeviceScanTestsParams);
+
+TYPED_TEST(RocprimDeviceScanTests, InclusiveScanSum)
+{
+    using T = typename TestFixture::input_type;
+    const bool debug_synchronous = TestFixture::debug_synchronous;
+
+    hc::accelerator acc;
+    hc::accelerator_view acc_view = acc.create_view();
+
+    const std::vector<size_t> sizes = get_sizes();
+    for(auto size : sizes)
+    {
+        SCOPED_TRACE(testing::Message() << "with size = " << size);
+
+        // Generate data
+        std::vector<T> input = get_random_data<T>(size, 1, 1);
+
+        hc::array<T> d_input(hc::extent<1>(size), input.begin(), acc_view);
+        hc::array<T> d_output(size, acc_view);
+        acc_view.wait();
+
+        // Calculate expected results on host
+        std::vector<T> expected(input.size(), 0);
+        std::partial_sum(input.begin(), input.end(), expected.begin());
+
+        // scan function
+        ::rocprim::plus<T> plus_op;
+        // temp storage
+        size_t temp_storage_size_bytes;
+        // Get size of d_temp_storage
+        rocprim::device_inclusive_scan(
+            nullptr,
+            temp_storage_size_bytes,
+            d_input.accelerator_pointer(),
+            d_output.accelerator_pointer(),
+            input.size(),
+            plus_op,
+            acc_view,
+            debug_synchronous
+        );
+        acc_view.wait();
+
+        // temp_storage_size_bytes must be >0
+        ASSERT_GT(temp_storage_size_bytes, 0);
+
+        // allocate temporary storage
+        hc::array<char> d_temp_storage(temp_storage_size_bytes, acc_view);
+        acc_view.wait();
+
+        // Run
+        rocprim::device_inclusive_scan(
+            d_temp_storage.accelerator_pointer(),
+            temp_storage_size_bytes,
+            d_input.accelerator_pointer(),
+            d_output.accelerator_pointer(),
+            input.size(),
+            plus_op,
+            acc_view,
+            debug_synchronous
+        );
+        acc_view.wait();
+
+        // Check if output values are as expected
+        std::vector<T> output = d_output;
+        for(size_t i = 0; i < output.size(); i++)
+        {
+            SCOPED_TRACE(testing::Message() << "where index = " << i);
+            ASSERT_EQ(output[i], expected[i]);
+        }
+    }
+}
+
+TYPED_TEST(RocprimDeviceScanTests, ExclusiveScanSum)
+{
+    using T = typename TestFixture::input_type;
+    const bool debug_synchronous = TestFixture::debug_synchronous;
+
+    hc::accelerator acc;
+    hc::accelerator_view acc_view = acc.create_view();
+
+    const std::vector<size_t> sizes = get_sizes();
+    for(auto size : sizes)
+    {
+        SCOPED_TRACE(testing::Message() << "with size = " << size);
+
+        // Generate data
+        std::vector<T> input = get_random_data<T>(size, 1, 1);
+
+        hc::array<T> d_input(hc::extent<1>(size), input.begin(), acc_view);
+        hc::array<T> d_output(size, acc_view);
+        acc_view.wait();
+
+        // Calculate expected results on host
+        std::vector<T> expected(input.size(), 0);
+        T initial_value = get_random_value<T>(1, 100);
+        input[0] += initial_value;
+        expected[0] = initial_value;
+        std::partial_sum(input.begin(), input.end() - 1, expected.begin() + 1);
+
+        // scan function
+        ::rocprim::plus<T> plus_op;
+        // temp storage
+        size_t temp_storage_size_bytes;
+        // Get size of d_temp_storage
+        rocprim::device_exclusive_scan(
+            nullptr, temp_storage_size_bytes,
+            d_input.accelerator_pointer(),
+            d_output.accelerator_pointer(),
+            initial_value,
+            input.size(),
+            plus_op,
+            acc_view,
+            debug_synchronous
+        );
+        acc_view.wait();
+
+        // temp_storage_size_bytes must be >0
+        ASSERT_GT(temp_storage_size_bytes, 0);
+
+        // allocate temporary storage
+        hc::array<char> d_temp_storage(temp_storage_size_bytes, acc_view);
+        acc_view.wait();
+
+        // Run
+        rocprim::device_exclusive_scan(
+            d_temp_storage.accelerator_pointer(),
+            temp_storage_size_bytes,
+            d_input.accelerator_pointer(),
+            d_output.accelerator_pointer(),
+            initial_value,
+            input.size(),
+            plus_op,
+            acc_view,
+            debug_synchronous
+        );
+        acc_view.wait();
+
+        // Check if output values are as expected
+        std::vector<T> output = d_output;
+        for(size_t i = 0; i < output.size(); i++)
+        {
+            SCOPED_TRACE(testing::Message() << "where index = " << i);
+            ASSERT_EQ(output[i], expected[i]);
+        }
+    }
+}
