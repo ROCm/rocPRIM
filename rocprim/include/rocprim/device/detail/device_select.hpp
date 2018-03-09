dc4e409642304016568b710809ec7e376160ad8f
Jakub Szuppe <jakub@streamhpc.com>
2018-03-09 10:35:59 +0100

    Add HC device select() func with select_op (test, benchmark)
    [ROCm/rocPRIM commit: 1392dd0cdb05d7fac5dae6a5da20bc6ac75e710a]

--- rocprim/include/rocprim/device/detail/device_select.hpp
+++ rocprim/include/rocprim/device/detail/device_select.hpp
@@ -141,6 +141,87 @@ void scatter_kernel_impl(InputIterator input,
     }
 }
 
+template<
+    unsigned int BlockSize,
+    unsigned int ItemsPerThread,
+    class InputIterator,
+    class SelectOp,
+    class OutputIterator,
+    class SelectedCountOutputIterator
+>
+ROCPRIM_DEVICE inline
+void scatter_if_kernel_impl(InputIterator input,
+                            size_t input_size,
+                            SelectOp select_op,
+                            unsigned int * output_indices,
+                            OutputIterator output,
+                            SelectedCountOutputIterator selected_count_output)
+{
+    using input_type = typename std::iterator_traits<InputIterator>::value_type;
+    constexpr unsigned int items_per_block = BlockSize * ItemsPerThread;
+
+    const unsigned int flat_id = ::rocprim::detail::block_thread_id<0>();
+    const unsigned int flat_block_id = ::rocprim::detail::block_id<0>();
+    const unsigned int block_offset = flat_block_id * items_per_block;
+    const unsigned int number_of_blocks = (input_size + items_per_block - 1)/items_per_block;
+    auto valid_in_last_block = input_size - items_per_block * (number_of_blocks - 1);
+
+    input_type values[ItemsPerThread];
+    unsigned int out_indices[ItemsPerThread];
+
+    // load input values into values
+    if(flat_block_id == (number_of_blocks - 1)) // last block
+    {
+        block_load_direct_striped<BlockSize>(
+            flat_id,
+            input + block_offset,
+            values,
+            valid_in_last_block
+        );
+        block_load_direct_striped<BlockSize>(
+            flat_id,
+            output_indices + block_offset,
+            out_indices,
+            valid_in_last_block
+        );
+    }
+    else
+    {
+        block_load_direct_striped<BlockSize>(
+            flat_id,
+            input + block_offset,
+            values
+        );
+        block_load_direct_striped<BlockSize>(
+            flat_id,
+            output_indices + block_offset,
+            out_indices
+        );
+    }
+
+    // Scatter
+    unsigned int index = block_offset + flat_id;
+    #pragma unroll
+    for(auto i = 0U; i < ItemsPerThread; i++)
+    {
+        if(index < input_size)
+        {
+            if(select_op(values[i]))
+            {
+                output[out_indices[i]] = values[i];
+            }
+        }
+        index += BlockSize;
+    }
+
+    // First thread in first block updates total number of selected values
+    if((flat_id + block_offset) == 0)
+    {
+        *selected_count_output =
+            output_indices[input_size - 1] + select_op(input[input_size - 1]);
+    }
+}
+
 } // end of detail namespace
 
 END_ROCPRIM_NAMESPACE
