9b3f6b944e016fd10e544c159772c01a46d61a75
Adel Johar <adel@streamhpc.com>
2018-03-20 12:36:31 +0400

    Implement block_histogram_atomic
    [ROCm/rocPRIM commit: 2402ebd7563cb9da02ce4b508b638f937e6e50e1]

---
+++ rocprim/include/rocprim/block/detail/block_histogram_atomic.hpp
@@ -0,0 +1,71 @@
+// Copyright (c) 2017 Advanced Micro Devices, Inc. All rights reserved.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+#ifndef ROCPRIM_BLOCK_DETAIL_BLOCK_HISTOGRAM_ATOMIC_HPP_
+#define ROCPRIM_BLOCK_DETAIL_BLOCK_HISTOGRAM_ATOMIC_HPP_
+
+#include <type_traits>
+
+#include "../../config.hpp"
+#include "../../detail/various.hpp"
+
+#include "../../intrinsics.hpp"
+#include "../../functional.hpp"
+
+BEGIN_ROCPRIM_NAMESPACE
+
+namespace detail
+{
+
+template<
+    class T,
+    unsigned int ItemsPerThread,
+    unsigned int Bins
+>
+class block_histogram_atomic
+{
+private:
+    using storage_type_ = typename ::rocprim::detail::empty_storage_type;
+
+public:
+    #ifndef DOXYGEN_SHOULD_SKIP_THIS // hides storage_type implementation for Doxygen
+    using storage_type = typename ::rocprim::detail::empty_storage_type;
+    #else
+    using storage_type = storage_type_; // only for Doxygen
+    #endif
+
+    template<class Counter>
+    ROCPRIM_DEVICE inline
+    void composite(T (&input)[ItemsPerThread],
+                   Counter (&hist)[Bins])
+    {
+        #pragma unroll
+        for (unsigned int i = 0; i < ItemsPerThread; ++i)
+        {
+              hc::atomic_fetch_add(hist + input[i], 1);
+        }
+    }
+};
+
+} // end namespace detail
+
+END_ROCPRIM_NAMESPACE
+
+#endif // ROCPRIM_BLOCK_DETAIL_BLOCK_HISTOGRAM_ATOMIC_HPP_
---
+++ test/rocprim/test_hc_block_histogram.cpp
@@ -0,0 +1,152 @@
+// MIT License
+//
+// Copyright (c) 2017 Advanced Micro Devices, Inc. All rights reserved.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#include <iostream>
+#include <vector>
+
+// Google Test
+#include <gtest/gtest.h>
+// HC API
+#include <hcc/hc.hpp>
+// rocPRIM API
+#include <rocprim/block/block_histogram.hpp>
+
+#include "test_utils.hpp"
+
+namespace rp = rocprim;
+
+// Params for tests
+template<
+    class T,
+    unsigned int BlockSize = 256U,
+    unsigned int ItemsPerThread = 1U,
+    rocprim::block_histogram_algorithm Algorithm = rocprim::block_histogram_algorithm::using_atomic
+>
+struct params
+{
+    using type = T;
+    static constexpr rocprim::block_histogram_algorithm algorithm = Algorithm;
+    static constexpr unsigned int block_size = BlockSize;
+    static constexpr unsigned int items_per_thread = ItemsPerThread;
+};
+
+template<class Params>
+class RocprimBlockHistogramInputArrayTests : public ::testing::Test
+{
+public:
+    using type = typename Params::type;
+    static constexpr unsigned int block_size = Params::block_size;
+    static constexpr rocprim::block_histogram_algorithm algorithm = Params::algorithm;
+    static constexpr unsigned int items_per_thread = Params::items_per_thread;
+};
+
+typedef ::testing::Types<
+    // -----------------------------------------------------------------------
+    // rocprim::block_histogram_algorithm::using_atomic
+    // -----------------------------------------------------------------------
+    params<unsigned int, 6U,   32>,
+    params<unsigned int, 32,   2>,
+    params<unsigned int, 256,  3>,
+    params<unsigned int, 512,  4>,
+    params<unsigned int, 1024, 1>,
+    params<unsigned int, 37,   2>,
+    params<unsigned int, 65,   5>,
+    params<unsigned int, 162,  7>,
+    params<unsigned int, 255,  15>
+> InputArrayTestParams;
+
+TYPED_TEST_CASE(RocprimBlockHistogramInputArrayTests, InputArrayTestParams);
+
+TYPED_TEST(RocprimBlockHistogramInputArrayTests, Histogram)
+{
+    using T = typename TestFixture::type;
+    constexpr auto algorithm = TestFixture::algorithm;
+    constexpr size_t block_size = TestFixture::block_size;
+    constexpr size_t items_per_thread = TestFixture::items_per_thread;
+    constexpr size_t bin = block_size;
+
+    hc::accelerator acc;
+    // Given block size not supported
+    if(block_size > test_utils::get_max_tile_size(acc))
+    {
+        return;
+    }
+
+    const size_t items_per_block = block_size * items_per_thread;
+    const size_t size = items_per_block * 37;
+    const size_t bin_sizes = bin * 37;
+    // Generate data
+    std::vector<T> output = test_utils::get_random_data<T>(size, 0, bin - 1);
+
+    // Output reduce results
+    std::vector<T> output_bin(bin_sizes, 0);
+
+    // Calculate expected results on host
+    std::vector<T> expected_bin(output_bin.size(), 0);
+    for(size_t i = 0; i < output.size() / items_per_block; i++)
+    {
+        for(size_t j = 0; j < items_per_block; j++)
+        {
+            auto bin_idx = i * bin;
+            auto idx = i * items_per_block + j;
+            expected_bin[bin_idx + output[idx]]++;
+        }
+    }
+
+    // global/grid size
+    const size_t global_size = output.size()/items_per_thread;
+    hc::array_view<T, 1> d_output(output.size(), output.data());
+    hc::array_view<T, 1> d_output_h(
+        output_bin.size(), output_bin.data()
+    );
+    hc::parallel_for_each(
+        acc.get_default_view(),
+        hc::extent<1>(global_size).tile(block_size),
+        [=](hc::tiled_index<1> i) [[hc]]
+        {
+            size_t idx = i.global[0] * items_per_thread;
+            tile_static T hist[bin];
+
+            // load
+            T in_out[items_per_thread];
+            for(unsigned int j = 0; j < items_per_thread; j++)
+            {
+                in_out[j] = d_output[idx + j];
+            }
+
+            rp::block_histogram<T, block_size, items_per_thread, bin, algorithm> bhist;
+            bhist.histogram(in_out, hist);
+            rp::syncthreads();
+            d_output_h[i.global[0]] = hist[i.local[0]];
+        }
+    );
+
+    d_output.synchronize();
+    d_output_h.synchronize();
+    for(size_t i = 0; i < output_bin.size(); i++)
+    {
+        ASSERT_NEAR(
+            output_bin[i], expected_bin[i],
+            static_cast<T>(0.05) * expected_bin[i]
+        );
+    }
+}
