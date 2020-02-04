fdf272e9fb02f3bb8b30059b5e7c37e069e14fdc
Eiden Yoshida <eiden.yoshida@amd.com>
2020-02-04 15:27:04 -0700

    Refactor Jenkinsfiles to use new docker containers

---
+++ .jenkins/common.groovy
@@ -0,0 +1,84 @@
+// This file is for internal AMD use.
+// If you are interested in running your own Jenkins, please raise a github issue for assistance.
+
+def runCompileCommand(platform, project)
+{
+    project.paths.construct_build_prefix()
+        
+    def command
+    
+    if(platform.jenkinsLabel.contains('hip-clang'))
+    {
+        command = """#!/usr/bin/env bash
+                set -x
+                cd ${project.paths.project_build_prefix}
+                LD_LIBRARY_PATH=/opt/rocm/hcc/lib CXX=/opt/rocm/bin/hipcc ${project.paths.build_command} --hip-clang
+            """
+    }
+    else
+    {
+        command = """#!/usr/bin/env bash
+                set -x
+                cd ${project.paths.project_build_prefix}
+                LD_LIBRARY_PATH=/opt/rocm/hcc/lib CXX=/opt/rocm/bin/hcc ${project.paths.build_command}
+            """
+    }
+    
+    platform.runCommand(this, command)
+}
+
+def runTestCommand (platform, project)
+{
+    String sudo = auxiliary.sudo(platform.jenkinsLabel)
+    def testCommand = 'ctest --output-on-failure'
+
+    def command = """#!/usr/bin/env bash
+                set -x
+                cd ${project.paths.project_build_prefix}
+                cd ${project.testDirectory}
+                ${sudo} LD_LIBRARY_PATH=/opt/rocm/lib ${testCommand}
+            """
+
+    platform.runCommand(this, command)
+}
+
+def runPackageCommand(platform, project)
+{
+    def command
+
+    if(platform.jenkinsLabel.contains('centos'))
+    {
+        command = """
+                set -x
+                cd ${project.paths.project_build_prefix}/${project.testDirectory}
+                make package
+                rm -rf package && mkdir -p package
+                mv *.rpm package/
+                rpm -qlp package/*.rpm
+            """
+
+        platform.runCommand(this, command)
+        platform.archiveArtifacts(this, """${project.paths.project_build_prefix}/${project.testDirectory}/package/*.rpm""")
+    }
+    else if(platform.jenkinsLabel.contains('hip-clang'))
+    {
+        packageCommand = null
+    }
+    else
+    {
+        command = """
+                set -x
+                cd ${project.paths.project_build_prefix}/${project.testDirectory}
+                make package
+                rm -rf package && mkdir -p package
+                mv *.deb package/
+                dpkg -c package/*.deb
+            """
+
+        platform.runCommand(this, command)
+        platform.archiveArtifacts(this, """${project.paths.project_build_prefix}/${project.testDirectory}/package/*.deb""")
+    }
+}
+
+return this
+
---
+++ .jenkins/precheckin.groovy
@@ -0,0 +1,81 @@
+#!/usr/bin/env groovy
+@Library('rocJenkins@pong') _
+import com.amd.project.*
+import com.amd.docker.*
+
+////////////////////////////////////////////////////////////////////////
+import java.nio.file.Path;
+
+def runCI = 
+{
+    nodeDetails, jobName->
+
+    def prj = new rocProject('rocPRIM')
+
+    def nodes = new dockerNodes(nodeDetails, jobName, prj)
+
+    boolean formatCheck = false
+     
+    def compileCommand =
+    {
+        platform, project->
+
+        commonGroovy = load "${project.paths.project_src_prefix}/.jenkins/common.groovy"
+        commonGroovy.runCompileCommand(platform, project)
+    }
+
+    def testCommand =
+    {
+        platform, project->
+
+        commonGroovy.runTestCommand(platform, project)
+    }
+
+    def packageCommand =
+    {
+        platform, project->
+
+        commonGroovy.runPackageCommand(platform, project)
+    }
+
+    buildProject(prj, formatCheck, nodes.dockerArray, compileCommand, testCommand, packageCommand)
+}
+
+ci: { 
+    String urlJobName = auxiliary.getTopJobName(env.BUILD_URL)
+
+    def propertyList = ["compute-rocm-dkms-no-npi":[pipelineTriggers([cron('0 1 * * 0')])], 
+                        "compute-rocm-dkms-no-npi-hipclang":[pipelineTriggers([cron('0 1 * * 0')])],
+                        "rocm-docker":[]]
+    propertyList = auxiliary.appendPropertyList(propertyList)
+
+    Set standardJobNameSet = ["compute-rocm-dkms-no-npi", "compute-rocm-dkms-no-npi-hipclang", "rocm-docker"]
+
+    def jobNameList = ["compute-rocm-dkms-no-npi":([ubuntu16:['gfx900'],centos7:['gfx906'],sles15sp1:['gfx908']]), 
+                       "compute-rocm-dkms-no-npi-hipclang":([ubuntu16:['gfx900'],centos7:['gfx906'],sles15sp1:['gfx908']]), 
+                       "rocm-docker":([ubuntu16:['gfx900'],centos7:['gfx906'],sles15sp1:['gfx908']])]
+    jobNameList = auxiliary.appendJobNameList(jobNameList)
+
+    propertyList.each 
+    {
+        jobName, property->
+        if (urlJobName == jobName)
+            properties(auxiliary.addCommonProperties(property))
+    }
+
+    Set seenJobNames = []
+    jobNameList.each 
+    {
+        jobName, nodeDetails->
+        seenJobNames.add(jobName)
+        if (urlJobName == jobName)
+            runCI(nodeDetails, jobName)
+    }
+
+    // For url job names that are outside of the standardJobNameSet i.e. compute-rocm-dkms-no-npi-1901
+    if(!seenJobNames.contains(urlJobName))
+    {
+        properties(auxiliary.addCommonProperties([pipelineTriggers([cron('0 1 * * *')])]))
+        runCI([centos7:['gfx906']], urlJobName)       
+    }
+}
