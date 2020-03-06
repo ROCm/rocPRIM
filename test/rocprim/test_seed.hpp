b772cde5d294f1cf8d9cacf2fd1dec3f0699b73e
Máté Ferenc Nagy-Egri <mate@streamhpc.com>
2020-03-06 12:06:59 +0000

    Reduce random test count

--- test/rocprim/test_seed.hpp
+++ test/rocprim/test_seed.hpp
@@ -21,8 +21,8 @@
 #ifndef TEST_SEED_HPP_
 #define TEST_SEED_HPP_
 
-static constexpr int random_seeds_count = 10;
-static constexpr unsigned int seeds [] = {0, 2, 10, 1000};
+static constexpr int random_seeds_count = 1;
+static constexpr unsigned int seeds [] = {0, 1000};
 static constexpr size_t seed_size = sizeof(seeds) / sizeof(seeds[0]);
 
 #endif // TEST_SEED_HPP_
