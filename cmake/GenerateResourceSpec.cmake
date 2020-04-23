169e1b7017ced1e9e92e03b0f0fe47ef5125b8be
Máté Ferenc Nagy-Egri <mate@streamhpc.com>
2020-04-23 15:36:54 +0000

    Finished GenerateResourceSpec.cmake
    [ROCm/rocPRIM commit: d1f7a0e26d574b57e8ff2d5a39fce21bdd056314]

--- cmake/GenerateResourceSpec.cmake
+++ cmake/GenerateResourceSpec.cmake
@@ -25,7 +25,27 @@ string(REGEX MATCHALL [[--(gfx[0-9]+)]]
   ${ROCMINFO_STDOUT}
 )
 
+# NOTE: Unfortunately we don't have structs in CMake,
+#       neither do we have std::partition only list(SORT)
+#
+
 # Transform raw regex matches to pairs of gfx IP and device id
+# This will be our struct emulation. In C++ it would be
+#
+# struct device
+# {
+#   std::string ip;
+#   int id;
+# };
+# 
+# std::vector<device> GFXIP_AND_ID{ {"gfx900",0},{"gfx803",1},{"gfx900",2} };
+# std::stable_sort(GFXIP_AND_ID.begin(), GFXIP_AND_ID.end(),
+#                  [](const device& lhs, const device& rhs)
+# {
+#   return std::lexicographical_compare(lhs.ip.begin(), lhs.ip.end(),
+#                                       rhs.ip.begin(), rhs.ip.end());
+# });
+#
 set(GFXIP_AND_ID)
 set(ID 0)
 foreach(ROCMINFO_MATCH IN LISTS ROCMINFO_MATCHES)
@@ -38,53 +58,77 @@ foreach(ROCMINFO_MATCH IN LISTS ROCMINFO_MATCHES)
   list(APPEND GFXIP_AND_ID "${ROCMINFO_MATCH}:${ID}")
   math(EXPR ID "${ID} + 1")
 endforeach()
-
-#
-# NOTE: Unfortunately we don't have std::partition in CMake, only list(SORT)
-#
-
-# Group together cards of matching gfx IP for JSON output
 list(SORT GFXIP_AND_ID)
 
 # Now comes the tricky part: implementing the following C++ logic
 #
-# stringstream JSON_PAYLOAD;
-# auto actual_gfx_ip = GFXIP_AND_ID.at(0).ip;
+# std::stringstream JSON_PAYLOAD;
 # auto it = GFXIP_AND_ID.begin();
-# while(it != GFXIP_AND_ID.end())
+# while (it != GFXIP_AND_ID.end())
 # {
-#   auto IT = find(it, GFXIP_AND_ID.end(),
-#                  [=](const tuple<string, int>& ip_id){ return ip_id.ip == actual_gfx_ip; });
-#   JSON_PAYLOAD << "      \"" << actual_gfx_ip << "\": [\n";
-#   foreach(it, IT, [&](const tuple<string, int>& ip_id)
-#   {
-#     JSON_PAYLOAD <<
-#       "        {\n" <<
-#       "           \"id\": \"" << ip_id.id << "\"\n" <<
-#       "        },\n"
-#   });
-#   JSON_PAYLOAD.get(); // discard trailing comma
-#   JSON_PAYLOAD << "      ],\n";
-#   it = IT;
+#     auto IT = std::find_if(it, GFXIP_AND_ID.end(),
+#                         [=](const device& ip_id){ return ip_id.ip.compare(it->ip) != 0; });
+#     JSON_PAYLOAD << "\n      \"" << it->ip << "\": [";
+#     std::for_each(it, IT, [&](const device& ip_id)
+#     {
+#       JSON_PAYLOAD <<
+#         "\n        {\n" <<
+#         "           \"id\": \"" << ip_id.id << "\"\n" <<
+#         "        },";
+#     });
+#     JSON_PAYLOAD.seekp(-1, std::ios_base::end); // discard trailing comma
+#     JSON_PAYLOAD << "\n      ],";
+#     it = IT;
 # }
-# JSON_PAYLOAD.get(); // discard trailing comma
+# JSON_PAYLOAD.seekp(-1, std::ios_base::end); // discard trailing comma
 #
+set(JSON_PAYLOAD)
+set(IT1 0)
+list(GET GFXIP_AND_ID ${IT1} I1)
+string(REGEX REPLACE ":[0-9]+" "" IP1 ${I1})
+list(LENGTH GFXIP_AND_ID COUNT)
+while(IT1 LESS COUNT)
+  string(APPEND JSON_PAYLOAD "\n      \"${IP1}\": [")
+  set(IT2 ${IT1})
+  list(GET GFXIP_AND_ID ${IT2} I2)
+  string(REGEX REPLACE [[:[0-9]+$]] "" IP2 ${I2})
+  string(REGEX REPLACE [[^gfx[0-9]+:]] "" ID2 ${I2})
+  while(${IP2} STREQUAL ${IP1} AND IT2 LESS COUNT)
+    string(APPEND JSON_PAYLOAD
+             "\n        {\n"
+             "           \"id\": \"${ID2}\"\n"
+             "        },"
+    )
+    math(EXPR IT2 "${IT2} + 1")
+    if(IT2 LESS COUNT)
+      list(GET GFXIP_AND_ID ${IT2} I2)
+      string(REGEX REPLACE [[:[0-9]+$]] "" IP2 ${I2})
+      string(REGEX REPLACE [[^gfx[0-9]+:]] "" ID2 ${I2})
+    endif()
+  endwhile()
+  string(REGEX REPLACE [[,$]] "" JSON_PAYLOAD ${JSON_PAYLOAD})
+  string(APPEND JSON_PAYLOAD "\n      ],")
+  set(IT1 ${IT2})
+endwhile()
+string(REGEX REPLACE [[,$]] "" JSON_PAYLOAD ${JSON_PAYLOAD})
 
-set()
-set(ID 0)
-list(GET GFXIP_AND_ID ${ID} )
-
-set(JSON_HEAD [[
-{
+set(JSON_HEAD [[{
   "version": {
     "major": 1,
     "minor": 0
   },
   "local": [
-    {
-]])
+    {]]
+)
 set(JSON_TAIL [[
+
     }
   ]
-}
-]])
+}]]
+)
+
+file(WRITE ${CMAKE_CURRENT_BINARY_DIR}/resources.json
+  ${JSON_HEAD}
+  ${JSON_PAYLOAD}
+  ${JSON_TAIL}
+)
