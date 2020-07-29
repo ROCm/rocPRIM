003947095a368bc499a67c677877d857c295a835
Saad Rahim <saad.rahim@amd.com>
2020-07-29 12:48:34 -0700

    Trying to fix

--- .jenkins/common.groovy
+++ .jenkins/common.groovy
@@ -5,7 +5,6 @@ def runCompileCommand(platform, project, jobName, boolean debug=false)
 {
     project.paths.construct_build_prefix()
         
-    project.paths.build_command = jobName.contains('hipclang') ? './install -c --hip-clang' : './install -c'
     project.compiler.compiler_path = platform.jenkinsLabel.contains('hip-clang') ? '/opt/rocm/bin/hipcc' : '/opt/rocm/bin/hcc'        
     String buildTypeArg = debug ? '-DCMAKE_BUILD_TYPE=Debug' : '-DCMAKE_BUILD_TYPE=Release'
     String buildTypeDir = debug ? 'debug' : 'release'
