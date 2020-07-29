cb8e4f7e706231b925b3cc2954d38dad125bf07a
Saad Rahim <saad.rahim@amd.com>
2020-07-29 12:29:10 -0700

    blah

--- .jenkins/staticlibrary.groovy
+++ .jenkins/staticlibrary.groovy
@@ -21,7 +21,7 @@ def runCI =
         platform, project->
 
         commonGroovy = load "${project.paths.project_src_prefix}/.jenkins/common.groovy"
-        commonGroovy.runCompileCommand(platform, project, jobName, false, true)
+        commonGroovy.runCompileCommand(platform, project, jobName, false)
     }
 
     
