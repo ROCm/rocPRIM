f194b1e9a83daed16c2969bb26f2f76812e82e36
Eiden Yoshida <eiden.yoshida@amd.com>
2021-04-06 14:26:17 -0600

    Only build for target of the CI node during PRs

--- .jenkins/precheckin.groovy
+++ .jenkins/precheckin.groovy
@@ -50,9 +50,8 @@ ci: {
                         "rocm-docker":[]]
     propertyList = auxiliary.appendPropertyList(propertyList)
 
-    def jobNameList = ["compute-rocm-dkms-no-npi":([ubuntu16:['gfx900'],centos7:['gfx906'],sles15sp1:['gfx908']]), 
-                       "compute-rocm-dkms-no-npi-hipclang":([ubuntu16:['gfx900'],centos7:['gfx906'],centos8:['gfx906'],sles15sp1:['gfx908']]), 
-                       "rocm-docker":([ubuntu16:['gfx900'],ubuntu18:['gfx906'],centos7:['gfx906'],sles15sp1:['gfx908']])]
+    def jobNameList = ["compute-rocm-dkms-no-npi-hipclang":([ubuntu18:['gfx900','gfx90a'],centos7:['gfx906'],centos8:['gfx906'],sles15sp1:['gfx908']]),
+                        "hipclang-bleeding-edge":([ubuntu18:['gfx900','gfx90a'],centos7:['gfx906'],centos8:['gfx906'],sles15sp1:['gfx908']])]
     jobNameList = auxiliary.appendJobNameList(jobNameList, 'rocPRIM')
 
     propertyList.each 
