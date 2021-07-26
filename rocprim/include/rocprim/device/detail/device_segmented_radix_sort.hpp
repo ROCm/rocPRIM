32a625dc1075ac46f62d41f4eb28449b064f979a
Stanley Tsang <stanley.tsang@amd.com>
2021-07-26 17:00:26 -0400

    Fixing macro

--- rocprim/include/rocprim/device/detail/device_segmented_radix_sort.hpp
+++ rocprim/include/rocprim/device/detail/device_segmented_radix_sort.hpp
@@ -43,9 +43,9 @@ BEGIN_ROCPRIM_NAMESPACE
 // local memory/register allocation errors during compilation.  Current workaround is to
 // use __attribute__((always_inline)) for the affected functions
 #ifdef WIN32
-#define ROCPRIM_SEGMENTED_RADIX_SORT_ALWAYS_INLINE __attribute__((always_inline))
-#elif
-#define ROCPRIM_SEGMENTED_RADIX_SORT_ALWAYS_INLINE
+#define ROCPRIM_SEGMENTED_RADIX_SORT_INLINE inline __attribute__((always_inline))
+#else
+#define ROCPRIM_SEGMENTED_RADIX_SORT_INLINE inline
 #endif
 namespace detail
 {
@@ -86,7 +86,7 @@ public:
         class ValuesInputIterator,
         class ValuesOutputIterator
     >
-    ROCPRIM_DEVICE inline  ROCPRIM_SEGMENTED_RADIX_SORT_ALWAYS_INLINE
+    ROCPRIM_DEVICE ROCPRIM_SEGMENTED_RADIX_SORT_INLINE
     void sort(KeysInputIterator keys_input,
               key_type * keys_tmp,
               KeysOutputIterator keys_output,
@@ -152,7 +152,7 @@ public:
     }
 
     // When all iterators are raw pointers, this overload is used to minimize code duplication in the kernel
-    ROCPRIM_DEVICE inline  ROCPRIM_SEGMENTED_RADIX_SORT_ALWAYS_INLINE
+    ROCPRIM_DEVICE ROCPRIM_SEGMENTED_RADIX_SORT_INLINE
     void sort(key_type * keys_input,
               key_type * keys_tmp,
               key_type * keys_output,
@@ -227,7 +227,7 @@ private:
         class ValuesInputIterator,
         class ValuesOutputIterator
     >
-    ROCPRIM_DEVICE inline  ROCPRIM_SEGMENTED_RADIX_SORT_ALWAYS_INLINE
+    ROCPRIM_DEVICE ROCPRIM_SEGMENTED_RADIX_SORT_INLINE
     void sort(KeysInputIterator keys_input,
               KeysOutputIterator keys_output,
               ValuesInputIterator values_input,
@@ -312,7 +312,7 @@ public:
         class ValuesInputIterator,
         class ValuesOutputIterator
     >
-    ROCPRIM_DEVICE inline  ROCPRIM_SEGMENTED_RADIX_SORT_ALWAYS_INLINE
+    ROCPRIM_DEVICE ROCPRIM_SEGMENTED_RADIX_SORT_INLINE
     void sort(KeysInputIterator keys_input,
               key_type * keys_tmp,
               KeysOutputIterator keys_output,
@@ -347,7 +347,7 @@ public:
     }
 
     // When all iterators are raw pointers, this overload is used to minimize code duplication in the kernel
-    ROCPRIM_DEVICE inline  ROCPRIM_SEGMENTED_RADIX_SORT_ALWAYS_INLINE
+    ROCPRIM_DEVICE ROCPRIM_SEGMENTED_RADIX_SORT_INLINE
     void sort(key_type * keys_input,
               key_type * keys_tmp,
               key_type * keys_output,
@@ -375,7 +375,7 @@ public:
         class ValuesInputIterator,
         class ValuesOutputIterator
     >
-    ROCPRIM_DEVICE inline  ROCPRIM_SEGMENTED_RADIX_SORT_ALWAYS_INLINE
+    ROCPRIM_DEVICE ROCPRIM_SEGMENTED_RADIX_SORT_INLINE
     bool sort(KeysInputIterator keys_input,
               KeysOutputIterator keys_output,
               ValuesInputIterator values_input,
@@ -457,7 +457,7 @@ public:
         class ValuesInputIterator,
         class ValuesOutputIterator
     >
-    ROCPRIM_DEVICE inline  ROCPRIM_SEGMENTED_RADIX_SORT_ALWAYS_INLINE
+    ROCPRIM_DEVICE ROCPRIM_SEGMENTED_RADIX_SORT_INLINE
     bool sort(KeysInputIterator,
               KeysOutputIterator,
               ValuesInputIterator,
@@ -483,7 +483,7 @@ template<
     class ValuesOutputIterator,
     class OffsetIterator
 >
-ROCPRIM_DEVICE inline  ROCPRIM_SEGMENTED_RADIX_SORT_ALWAYS_INLINE
+ROCPRIM_DEVICE ROCPRIM_SEGMENTED_RADIX_SORT_INLINE
 void segmented_sort(KeysInputIterator keys_input,
                     typename std::iterator_traits<KeysInputIterator>::value_type * keys_tmp,
                     KeysOutputIterator keys_output,
