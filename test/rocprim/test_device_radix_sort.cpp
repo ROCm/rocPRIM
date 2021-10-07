63a0a93981205565d5011b8780265d21b7624855
Stanley Tsang <stanley.tsang@amd.com>
2021-10-07 01:40:12 -0400

    Cherry picking bfloat16 radix sort test for 4.5

--- test/rocprim/test_device_radix_sort.cpp
+++ test/rocprim/test_device_radix_sort.cpp
@@ -62,13 +62,11 @@ typedef ::testing::Types<
     params<double, int, true>,
     params<float, int>,
     params<rocprim::half, long long>,
-    //TODO: Disable bfloat16 test until we get a better bfloat16 implemetation for host side
-    //params<rocprim::bfloat16, long long>,
+    params<rocprim::bfloat16, long long>,
     params<int8_t, int8_t>,
     params<uint8_t, uint8_t>,
     params<rocprim::half, rocprim::half>,
-    //TODO: Disable bfloat16 test until we get a better bfloat16 implemetation for host side
-    //params<rocprim::bfloat16, rocprim::bfloat16>,
+    params<rocprim::bfloat16, rocprim::bfloat16>,
     params<int, test_utils::custom_test_type<float>>,
 
     // start_bit and end_bit
@@ -80,9 +78,8 @@ typedef ::testing::Types<
     params<unsigned int, double, true, 4, 21>,
     params<unsigned int, rocprim::half, true, 0, 15>,
     params<unsigned short, rocprim::half, false, 3, 22>,
-    //TODO: Disable bfloat16 test until we get a better bfloat16 implemetation for host side
-    //params<unsigned int, rocprim::bfloat16, true, 0, 15>,
-    //params<unsigned short, rocprim::bfloat16, false, 3, 22>,
+    params<unsigned int, rocprim::bfloat16, true, 0, 12>,
+    params<unsigned short, rocprim::bfloat16, false, 3, 11>,
     params<unsigned long long, char, false, 8, 20>,
     params<unsigned short, test_utils::custom_test_type<double>, false, 8, 11>,
 
@@ -292,7 +289,7 @@ TYPED_TEST(RocprimDeviceRadixSort, SortPairs)
             }
 
             std::vector<value_type> values_input(size);
-            std::iota(values_input.begin(), values_input.end(), 0);
+            test_utils::iota(values_input.begin(), values_input.end(), 0);
 
             key_type * d_keys_input;
             key_type * d_keys_output;
@@ -599,7 +596,7 @@ TYPED_TEST(RocprimDeviceRadixSort, SortPairsDoubleBuffer)
             }
 
             std::vector<value_type> values_input(size);
-            std::iota(values_input.begin(), values_input.end(), 0);
+            test_utils::iota(values_input.begin(), values_input.end(), 0);
 
             key_type * d_keys_input;
             key_type * d_keys_output;
