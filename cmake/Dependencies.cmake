ac1c724683f4e851b3b076af6cd83535529155bc
pavahora <92880691+pavahora@users.noreply.github.com>
2021-11-10 17:14:31 -0600

    Update to GoogleTest 1.11.0 (#287)
    Co-authored-by: Vahora <pavahora@amd.com>

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -129,7 +129,7 @@ if(BUILD_TEST)
     download_project(
       PROJ                googletest
       GIT_REPOSITORY      https://github.com/google/googletest.git
-      GIT_TAG             release-1.10.0
+      GIT_TAG             release-1.11.0
       INSTALL_DIR         ${GTEST_ROOT}
       CMAKE_ARGS          -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} -DBUILD_SHARED_LIBS=${BUILD_SHARED_LIBS} -DBUILD_GTEST=ON -DINSTALL_GTEST=ON -Dgtest_force_shared_crt=ON -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>
       LOG_DOWNLOAD        TRUE
