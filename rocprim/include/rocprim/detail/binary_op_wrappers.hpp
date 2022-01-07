5595e0602cd316a15bfb4b1876375e446188b790
Gergely Meszaros <gergely@streamhpc.com>
2022-01-07 12:45:43 +0000

    Optimize headflag_scan_op_wrapper
    The code should be functionally equivalent.
    This improves performance of scan_by_key.

--- rocprim/include/rocprim/detail/binary_op_wrappers.hpp
+++ rocprim/include/rocprim/detail/binary_op_wrappers.hpp
@@ -91,14 +91,10 @@ struct headflag_scan_op_wrapper
     ROCPRIM_HOST_DEVICE inline
     result_type operator()(const input_type& t1, const input_type& t2)
     {
-        if(!rocprim::get<1>(t2))
-        {
-            return rocprim::make_tuple(
-                scan_op_(rocprim::get<0>(t1), rocprim::get<0>(t2)),
-                static_cast<F>(rocprim::get<1>(t1) || rocprim::get<1>(t2))
-            );
-        }
-        return t2;
+        return rocprim::make_tuple(!rocprim::get<1>(t2)
+                                       ? scan_op_(rocprim::get<0>(t1), rocprim::get<0>(t2))
+                                       : rocprim::get<0>(t2),
+                                   F {rocprim::get<1>(t2) || rocprim::get<1>(t1)});
     }
 
 private:
