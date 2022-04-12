387c28ad202b7f8cf890c60c47a0ebffd3c6da8f
Gergely Meszaros <gergely@streamhpc.com>
2022-04-12 12:54:54 +0000

    Add code format check script and git hook [CI skip]
    The script by default checks diffs for code formatting violations
    compared to the default branch (develop_stream).
    The commit hook checks the current commit. It can be installed by running
    the `install` script in .githooks.

---
+++ scripts/code-format/check-format.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+SOURCE_COMMIT="$1"
+if [ "$#" -gt 0 ]; then
+    shift
+fi
+
+# If no source commit is given target the default branch
+if [ "x$SOURCE_COMMIT" = "x" ]; then
+    # If remote is not set use the remote of the current branch or fallback to "origin"
+    if [ "x$REMOTE" = "x" ]; then
+        BRANCH="$(git rev-parse --abbrev-ref HEAD)"
+        REMOTE="$(git config --local --get "branch.$BRANCH.remote" || echo 'origin')"
+    fi
+    SOURCE_COMMIT="remotes/$REMOTE/HEAD"
+fi
+
+# Force colored diff output
+DIFF_COLOR_SAVED="$(git config --local --get diff.color)"
+if [ "x$DIFF_COLOR_SAVED" != "x" ]; then
+    git config --local --replace-all "diff.color" "always"
+else
+    git config --local --add "diff.color" "always"
+fi
+
+GIT_CLANG_FORMAT="${GIT_CLANG_FORMAT:-git-clang-format}"
+CLANG_FORMAT_OUTPUT="$("$GIT_CLANG_FORMAT" --style=file --diff "$@" "$SOURCE_COMMIT")"
+
+# Restore setting
+if [ "x$DIFF_COLOR_SAVED" != "x" ]; then
+    git config --local --replace-all "diff.color" "$DIFF_COLOR_SAVED"
+else
+    git config --local --unset "diff.color"
+fi
+
+# Check for no-ops
+echo "$CLANG_FORMAT_OUTPUT" | grep '^no modified files to format$'           > /dev/null && exit 0
+echo "$CLANG_FORMAT_OUTPUT" | grep '^clang-format did not modify any files$' > /dev/null && exit 0
+
+# Dump formatting diff and signal failure
+printf \
+"\033[31m==== FORMATTING VIOLATIONS DETECTED ====\033[0m
+run '\033[33m%s --style=file %s %s'\033[0m to apply these formating changes\n\n" \
+"$GIT_CLANG_FORMAT" "$@" "$SOURCE_COMMIT"
+
+echo "$CLANG_FORMAT_OUTPUT"
+exit 1
