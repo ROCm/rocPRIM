f4cccaf60ac2d959b92c093d2e1b0610b9242402
Nol Moonen <nol@streamhpc.com>
2022-04-20 11:59:09 +0000

    applied matrix generation to several device algorithm benchmarks
    remove filter in benchmark config tuning

--- benchmark/benchmark_device_radix_sort_single.cpp
+++ benchmark/benchmark_device_radix_sort_single.cpp
@@ -1,6 +1,6 @@
 // MIT License
 //
-// Copyright (c) 2017-2021 Advanced Micro Devices, Inc. All rights reserved.
+// Copyright (c) 2017-2022 Advanced Micro Devices, Inc. All rights reserved.
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -20,216 +20,41 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-#include "benchmark_device_radix_sort.hpp"
+#include <cstddef>
+#include <string>
+
+// Google Benchmark
+#include <benchmark/benchmark.h>
+
+// HIP API
+#include <hip/hip_runtime.h>
+
+// CmdParser
+#include "cmdparser.hpp"
+
+#include "benchmark_device_radix_sort.parallel.hpp"
+#include "benchmark_utils.hpp"
 
 #ifndef DEFAULT_N
 const size_t DEFAULT_N = 1024;
 #endif
-
-#ifdef BENCHMARK_CONFIG_TUNING
-
-template<
-    typename Key, typename Value,
-    unsigned int BlockSize, unsigned int ItemsPerThread
->
-auto sort_keys_add_benchmark(
-    std::vector<benchmark::internal::Benchmark*>& benchmarks,
-    hipStream_t stream,
-    size_t size)
-    -> typename std::enable_if<
-        std::is_same<Value, ::rocprim::empty_type>::value, void
-    >::type
-{
-    benchmarks.push_back(
-        benchmark::RegisterBenchmark(
-            (std::string("sort_keys") + "<" + std::string(Traits<Key>::name()) + ", radix_sort_single_config<" +
-             ", kernel_config<" + std::to_string(BlockSize) + ", " + std::to_string(ItemsPerThread) + ">>>").c_str(),
-            [=](benchmark::State& state) {
-                run_sort_keys_benchmark<
-                    Key,
-                    rocprim::radix_sort_config<
-                        8,
-                        7,
-                        rocprim::kernel_config<256U, 2>,
-                        rocprim::kernel_config<256U, 10>,
-                        rocprim::kernel_config<BlockSize, ItemsPerThread>,
-                        rocprim::kernel_config<1024, 1>,
-                        1024U,
-                        true
-                    >
-                >(state, stream, size);
-            }
-        )
-    );
-}
-
-template<
-    typename Key, typename Value,
-    unsigned int BlockSize, unsigned int ItemsPerThread
->
-auto sort_keys_add_benchmark(
-    std::vector<benchmark::internal::Benchmark*>& benchmarks,
-    hipStream_t stream,
-    size_t size)
-    -> typename std::enable_if<
-        !std::is_same<Value, ::rocprim::empty_type>::value, void
-    >::type
-{
-    benchmarks.push_back(
-        benchmark::RegisterBenchmark(
-            (std::string("sort_pairs") + "<" + std::string(Traits<Key>::name()) + "," + std::string(Traits<Value>::name()) +
-             ", kernel_config<" + std::to_string(BlockSize) + ", " + std::to_string(ItemsPerThread) + ">>>").c_str(),
-            [=](benchmark::State& state) {
-                run_sort_pairs_benchmark<
-                    Key, Value,
-                    rocprim::radix_sort_config<
-                        8,
-                        7,
-                        rocprim::kernel_config<256U, 2>,
-                        rocprim::kernel_config<256U, 10>,
-                        rocprim::kernel_config<BlockSize, ItemsPerThread>,
-                        rocprim::kernel_config<1024, 1>,
-                        1024U,
-                        true
-                    >
-                >(state, stream, size);
-            }
-        )
-    );
-}
-
-template<
-    typename Key, typename Value,
-    unsigned int BlockSize, unsigned int ItemsPerThread,
-    unsigned int MaxItemsPerThread
->
-auto sort_keys_benchmark_generate_ipt_grid(
-    std::vector<benchmark::internal::Benchmark*>& benchmarks,
-    hipStream_t stream,
-    size_t size)
-    -> typename std::enable_if< ItemsPerThread == MaxItemsPerThread, void>::type
-{
-    sort_keys_add_benchmark<
-        Key, Value,
-        BlockSize, ItemsPerThread
-    >(benchmarks, stream, size);
-}
-
-template<
-    typename Key, typename Value,
-    unsigned int BlockSize, unsigned int ItemsPerThread,
-    unsigned int MaxItemsPerThread
->
-auto sort_keys_benchmark_generate_ipt_grid(
-    std::vector<benchmark::internal::Benchmark*>& benchmarks,
-    hipStream_t stream,
-    size_t size)
-    -> typename std::enable_if< ItemsPerThread < MaxItemsPerThread, void>::type
-{
-    sort_keys_add_benchmark<
-        Key, Value,
-        BlockSize, ItemsPerThread
-    >(benchmarks, stream, size);
-
-    sort_keys_benchmark_generate_ipt_grid<
-        Key, Value,
-        BlockSize, ItemsPerThread + 1,
-        MaxItemsPerThread
-    >(benchmarks, stream, size);
-}
-
-template<
-    typename Key, typename Value = ::rocprim::empty_type,
-    unsigned int MaxItemsPerThread = 10U
->
-void sort_keys_benchmark_generate(
-    std::vector<benchmark::internal::Benchmark*>& benchmarks,
-    hipStream_t stream,
-    size_t size)
-{
-    sort_keys_benchmark_generate_ipt_grid<
-        Key, Value,
-        64U, // BlockSize
-        1U,  // ItemsPerThread start value
-        MaxItemsPerThread
-    >(benchmarks, stream, size);
-
-    sort_keys_benchmark_generate_ipt_grid<
-        Key, Value,
-        128U, // BlockSize
-        1U,   // ItemsPerThread start value
-        MaxItemsPerThread
-    >(benchmarks, stream, size);
-
-    sort_keys_benchmark_generate_ipt_grid<
-        Key, Value,
-        256U, // BlockSize
-        1U,   // ItemsPerThread start value
-        MaxItemsPerThread
-    >(benchmarks, stream, size);
-
-    sort_keys_benchmark_generate_ipt_grid<
-        Key, Value,
-        512U, // BlockSize
-        1U,   // ItemsPerThread start value
-        7U    // MaxItemsPerThread
-    >(benchmarks, stream, size);
-
-    sort_keys_benchmark_generate_ipt_grid<
-        Key, Value,
-        1024U, // BlockSize
-        1U,    // ItemsPerThread start value
-        1U     // MaxItemsPerThread
-    >(benchmarks, stream, size);
-}
-
-// Compilation may never finish, if the compiler needs to compile too many kernels,
-// it is recommended to compile benchmarks only for 1-2 types when BENCHMARK_CONFIG_TUNING is used
-// (all other sort_keys_benchmark_generate should be commented/removed).
-void add_sort_keys_benchmarks(std::vector<benchmark::internal::Benchmark*>& benchmarks,
-                              hipStream_t stream,
-                              size_t size)
-{
-    sort_keys_benchmark_generate<int>(benchmarks, stream, size);
-    sort_keys_benchmark_generate<long long>(benchmarks, stream, size);
-    sort_keys_benchmark_generate<int8_t>(benchmarks, stream, size);
-    //sort_keys_benchmark_generate<uint8_t>(benchmarks, stream, size);
-    sort_keys_benchmark_generate<rocprim::half, ::rocprim::empty_type, 30>(benchmarks, stream, size);
-    sort_keys_benchmark_generate<short>(benchmarks, stream, size);
-}
-
-void add_sort_pairs_benchmarks(std::vector<benchmark::internal::Benchmark*>& benchmarks,
-                               hipStream_t stream,
-                               size_t size)
-{
-    //using custom_float2 = custom_type<float, float>;
-    using custom_double2 = custom_type<double, double>;
-
-    sort_keys_benchmark_generate<int, float>(benchmarks, stream, size);
-    sort_keys_benchmark_generate<int, double>(benchmarks, stream, size);
-    //sort_keys_benchmark_generate<int, float2, 15>(benchmarks, stream, size);
-    //sort_keys_benchmark_generate<int, custom_float2, 15>(benchmarks, stream, size);
-    //sort_keys_benchmark_generate<int, double2, 15>(benchmarks, stream, size);
-    sort_keys_benchmark_generate<int, custom_double2, 15>(benchmarks, stream, size);
-
-    sort_keys_benchmark_generate<long long, float>(benchmarks, stream, size);
-    sort_keys_benchmark_generate<long long, double>(benchmarks, stream, size);
-    //sort_keys_benchmark_generate<long long, float2, 15>(benchmarks, stream, size);
-    //sort_keys_benchmark_generate<long long, custom_float2, 15>(benchmarks, stream, size);
-    //sort_keys_benchmark_generate<long long, double2, 15>(benchmarks, stream, size);
-    sort_keys_benchmark_generate<long long, custom_double2, 15>(benchmarks, stream, size);
-    sort_keys_benchmark_generate<int8_t, int8_t>(benchmarks, stream, size);
-    sort_keys_benchmark_generate<uint8_t, uint8_t>(benchmarks, stream, size);
-    sort_keys_benchmark_generate<rocprim::half, rocprim::half, 30>(benchmarks, stream, size);
-}
-
-#endif // BENCHMARK_CONFIG_TUNING
 
 int main(int argc, char *argv[])
 {
     cli::Parser parser(argc, argv);
     parser.set_optional<size_t>("size", "size", DEFAULT_N, "number of values");
     parser.set_optional<int>("trials", "trials", -1, "number of iterations");
+#ifdef BENCHMARK_CONFIG_TUNING
+    // optionally run an evenly split subset of benchmarks, when making multiple program invocations
+    parser.set_optional<int>("parallel_instance",
+                             "parallel_instance",
+                             0,
+                             "parallel instance index");
+    parser.set_optional<int>("parallel_instances",
+                             "parallel_instances",
+                             1,
+                             "total parallel instances");
+#endif
     parser.run_and_exit_if_error();
 
     // Parse argv
@@ -245,9 +70,21 @@ int main(int argc, char *argv[])
     benchmark::AddCustomContext("size", std::to_string(size));
 
     // Add benchmarks
-    std::vector<benchmark::internal::Benchmark*> benchmarks;
+    std::vector<benchmark::internal::Benchmark*> benchmarks = {};
+#ifdef BENCHMARK_CONFIG_TUNING
+    const int parallel_instance  = parser.get<int>("parallel_instance");
+    const int parallel_instances = parser.get<int>("parallel_instances");
+    config_autotune_register::register_benchmark_subset(benchmarks,
+                                                        parallel_instance,
+                                                        parallel_instances,
+                                                        size,
+                                                        stream);
+    benchmark::AddCustomContext("autotune_config_pattern",
+                                device_radix_sort_benchmark<>::get_name_pattern().c_str());
+#else // BENCHMARK_CONFIG_TUNING
     add_sort_keys_benchmarks(benchmarks, stream, size);
     add_sort_pairs_benchmarks(benchmarks, stream, size);
+#endif // BENCHMARK_CONFIG_TUNING
 
     // Use manual timing
     for(auto& b : benchmarks)
