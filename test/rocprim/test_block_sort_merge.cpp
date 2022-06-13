18d61b377ba35e81bdd26fad9fcfdab31553afce
Vince van Heertum <vince@streamhpc.com>
2022-06-13 13:20:36 +0000

    Resolve "Implement the block level merge path kernel for merge"

--- test/rocprim/test_block_sort_merge.cpp
+++ test/rocprim/test_block_sort_merge.cpp
@@ -1,6 +1,6 @@
 // MIT License
 //
-// Copyright (c) 2017-2020 Advanced Micro Devices, Inc. All rights reserved.
+// Copyright (c) 2022 Advanced Micro Devices, Inc. All rights reserved.
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -20,12 +20,12 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-#include "common_test_header.hpp"
+#include "../common_test_header.hpp"
 
 // required rocprim headers
 #include <rocprim/block/block_load.hpp>
-#include <rocprim/block/block_store.hpp>
 #include <rocprim/block/block_sort.hpp>
+#include <rocprim/block/block_store.hpp>
 
 // required test headers
 #include "test_utils_types.hpp"
@@ -34,10 +34,12 @@
 #include "test_block_sort.kernels.hpp"
 
 // Start stamping out tests
-struct RocprimBlockSortTests;
-
+struct RocprimBlockMergeSortTests;
+#ifndef TEST_BLOCK_SORT_ALGORITHM
+    #define TEST_BLOCK_SORT_ALGORITHM rocprim::block_sort_algorithm::merge_sort
+#endif
 struct Integral;
-#define suite_name RocprimBlockSortTests
+#define suite_name RocprimBlockMergeSortTests
 #define block_params BlockParamsIntegral
 #define name_suffix Integral
 
@@ -48,7 +50,7 @@ struct Integral;
 #undef name_suffix
 
 struct Floating;
-#define suite_name RocprimBlockSortTests
+#define suite_name RocprimBlockMergeSortTests
 #define block_params BlockParamsFloating
 #define name_suffix Floating
 
