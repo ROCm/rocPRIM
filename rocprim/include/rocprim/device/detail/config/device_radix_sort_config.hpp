9bd6c2f8f1c934e6269a2c1a6d5c550b48f9ab2b
Vince van Heertum <vince@streamhpc.com>
2022-08-06 01:25:41 +0200

    Seperate default configs from other config-related code (e.g. config wrappers and dynamic dispatch stuff).

---
+++ rocprim/include/rocprim/device/detail/config/device_radix_sort_config.hpp
@@ -0,0 +1,370 @@
+// Copyright (c) 2018-2022 Advanced Micro Devices, Inc. All rights reserved.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+#ifndef ROCPRIM_DEVICE_DETAIL_CONFIG_DEVICE_RADIX_SORT_HPP_
+#define ROCPRIM_DEVICE_DETAIL_CONFIG_DEVICE_RADIX_SORT_HPP_
+
+#include <type_traits>
+
+#include "../../../config.hpp"
+#include "../../../detail/various.hpp"
+#include "../../../functional.hpp"
+
+#include "../../config_types.hpp"
+#include "../device_config_helper.hpp"
+
+/// \addtogroup primitivesmodule_deviceconfigs
+/// @{
+
+BEGIN_ROCPRIM_NAMESPACE
+
+namespace detail
+{
+
+template<class Key, class Value>
+struct radix_sort_config_803
+{
+    static constexpr unsigned int item_scale = ::rocprim::detail::ceiling_div<unsigned int>(
+        ::rocprim::max(sizeof(Key), sizeof(Value)), sizeof(int));
+
+    using scan = kernel_config<256, 2>;
+
+    using type = select_type<
+        select_type_case<
+            (sizeof(Key) == 1 && sizeof(Value) <= 8),
+            radix_sort_config<8, 7, scan, kernel_config<256, 10>, kernel_config<256, 19>>>,
+        select_type_case<
+            (sizeof(Key) == 2 && sizeof(Value) <= 8),
+            radix_sort_config<8, 7, scan, kernel_config<256, 10>, kernel_config<256, 17>>>,
+        select_type_case<
+            (sizeof(Key) == 4 && sizeof(Value) <= 8),
+            radix_sort_config<7, 6, scan, kernel_config<256, 15>, kernel_config<256, 13>>>,
+        select_type_case<
+            (sizeof(Key) == 8 && sizeof(Value) <= 8),
+            radix_sort_config<7, 6, scan, kernel_config<256, 13>, kernel_config<256, 10>>>,
+        radix_sort_config<
+            6,
+            4,
+            scan,
+            kernel_config<limit_block_size<256U, sizeof(Value), ROCPRIM_WARP_SIZE_64>::value,
+                          ::rocprim::max(1u, 15u / item_scale)>,
+            kernel_config<limit_block_size<256U, sizeof(Value), ROCPRIM_WARP_SIZE_64>::value,
+                          ::rocprim::max(1u, 10u / item_scale)>,
+            kernel_config<limit_block_size<256U, sizeof(Value), ROCPRIM_WARP_SIZE_64>::value,
+                          ::rocprim::max(1u, 10u / item_scale)>>>;
+};
+
+template<class Key>
+struct radix_sort_config_803<Key, empty_type>
+    : select_type<select_type_case<sizeof(Key) == 1,
+                                   radix_sort_config<8,
+                                                     7,
+                                                     kernel_config<256, 2>,
+                                                     kernel_config<256, 10>,
+                                                     kernel_config<256, 19>>>,
+                  select_type_case<sizeof(Key) == 2,
+                                   radix_sort_config<8,
+                                                     7,
+                                                     kernel_config<256, 2>,
+                                                     kernel_config<256, 10>,
+                                                     kernel_config<256, 16>>>,
+                  select_type_case<sizeof(Key) == 4,
+                                   radix_sort_config<7,
+                                                     6,
+                                                     kernel_config<256, 2>,
+                                                     kernel_config<256, 9>,
+                                                     kernel_config<256, 15>>>,
+                  select_type_case<sizeof(Key) == 8,
+                                   radix_sort_config<7,
+                                                     6,
+                                                     kernel_config<256, 2>,
+                                                     kernel_config<256, 7>,
+                                                     kernel_config<256, 12>>>>
+{};
+
+template<class Key, class Value>
+struct radix_sort_config_900
+{
+    static constexpr unsigned int item_scale = ::rocprim::detail::ceiling_div<unsigned int>(
+        ::rocprim::max(sizeof(Key), sizeof(Value)), sizeof(int));
+
+    using scan = kernel_config<256, 2>;
+
+    using type = select_type<
+        select_type_case<
+            (sizeof(Key) == 1 && sizeof(Value) <= 8),
+            radix_sort_config<4, 4, scan, kernel_config<256, 10>, kernel_config<256, 19>>>,
+        select_type_case<
+            (sizeof(Key) == 2 && sizeof(Value) <= 8),
+            radix_sort_config<6, 5, scan, kernel_config<256, 10>, kernel_config<256, 17>>>,
+        select_type_case<
+            (sizeof(Key) == 4 && sizeof(Value) <= 8),
+            radix_sort_config<7, 6, scan, kernel_config<256, 15>, kernel_config<256, 15>>>,
+        select_type_case<
+            (sizeof(Key) == 8 && sizeof(Value) <= 8),
+            radix_sort_config<7, 6, scan, kernel_config<256, 15>, kernel_config<256, 12>>>,
+        radix_sort_config<
+            6,
+            4,
+            scan,
+            kernel_config<limit_block_size<256U, sizeof(Value), ROCPRIM_WARP_SIZE_64>::value,
+                          ::rocprim::max(1u, 15u / item_scale)>,
+            kernel_config<limit_block_size<256U, sizeof(Value), ROCPRIM_WARP_SIZE_64>::value,
+                          ::rocprim::max(1u, 10u / item_scale)>,
+            kernel_config<limit_block_size<256U, sizeof(Value), ROCPRIM_WARP_SIZE_64>::value,
+                          ::rocprim::max(1u, 10u / item_scale)>>>;
+};
+
+template<class Key>
+struct radix_sort_config_900<Key, empty_type>
+    : select_type<select_type_case<sizeof(Key) == 1,
+                                   radix_sort_config<4,
+                                                     3,
+                                                     kernel_config<256, 2>,
+                                                     kernel_config<256, 10>,
+                                                     kernel_config<256, 19>>>,
+                  select_type_case<sizeof(Key) == 2,
+                                   radix_sort_config<6,
+                                                     5,
+                                                     kernel_config<256, 2>,
+                                                     kernel_config<256, 10>,
+                                                     kernel_config<256, 16>>>,
+                  select_type_case<sizeof(Key) == 4,
+                                   radix_sort_config<7,
+                                                     6,
+                                                     kernel_config<256, 2>,
+                                                     kernel_config<256, 17>,
+                                                     kernel_config<256, 15>>>,
+                  select_type_case<sizeof(Key) == 8,
+                                   radix_sort_config<7,
+                                                     6,
+                                                     kernel_config<256, 2>,
+                                                     kernel_config<256, 15>,
+                                                     kernel_config<256, 12>>>>
+{};
+
+template<class Key, class Value>
+struct radix_sort_config_908
+{
+    static constexpr unsigned int item_scale = ::rocprim::detail::ceiling_div<unsigned int>(
+        ::rocprim::max(sizeof(Key), sizeof(Value)), sizeof(int));
+
+    using scan = kernel_config<256, 2>;
+
+    using type = select_type<
+        select_type_case<
+            (sizeof(Key) == 1 && sizeof(Value) <= 8),
+            radix_sort_config<4, 4, scan, kernel_config<256, 10>, kernel_config<256, 19>>>,
+        select_type_case<
+            (sizeof(Key) == 2 && sizeof(Value) <= 8),
+            radix_sort_config<6, 5, scan, kernel_config<256, 10>, kernel_config<256, 17>>>,
+        select_type_case<(sizeof(Key) == 4 && sizeof(Value) <= 8),
+                         radix_sort_config<7,
+                                           6,
+                                           kernel_config<256, 4>,
+                                           kernel_config<256, 15>,
+                                           kernel_config<256, 15>>>,
+        select_type_case<(sizeof(Key) == 8 && sizeof(Value) <= 8),
+                         radix_sort_config<7,
+                                           6,
+                                           kernel_config<256, 4>,
+                                           kernel_config<256, 14>,
+                                           kernel_config<256, 12>>>,
+        radix_sort_config<
+            6,
+            4,
+            scan,
+            kernel_config<limit_block_size<256U, sizeof(Value), ROCPRIM_WARP_SIZE_64>::value,
+                          ::rocprim::max(1u, 15u / item_scale)>,
+            kernel_config<limit_block_size<256U, sizeof(Value), ROCPRIM_WARP_SIZE_64>::value,
+                          ::rocprim::max(1u, 10u / item_scale)>,
+            kernel_config<limit_block_size<256U, sizeof(Value), ROCPRIM_WARP_SIZE_64>::value,
+                          ::rocprim::max(1u, 10u / item_scale)>>>;
+};
+
+template<class Key>
+struct radix_sort_config_908<Key, empty_type>
+    : select_type<select_type_case<sizeof(Key) == 1,
+                                   radix_sort_config<4,
+                                                     3,
+                                                     kernel_config<256, 2>,
+                                                     kernel_config<256, 10>,
+                                                     kernel_config<256, 19>>>,
+                  select_type_case<sizeof(Key) == 2,
+                                   radix_sort_config<6,
+                                                     5,
+                                                     kernel_config<256, 2>,
+                                                     kernel_config<256, 10>,
+                                                     kernel_config<256, 17>>>,
+                  select_type_case<sizeof(Key) == 4,
+                                   radix_sort_config<7,
+                                                     6,
+                                                     kernel_config<256, 4>,
+                                                     kernel_config<256, 17>,
+                                                     kernel_config<256, 15>>>,
+                  select_type_case<sizeof(Key) == 8,
+                                   radix_sort_config<7,
+                                                     6,
+                                                     kernel_config<256, 4>,
+                                                     kernel_config<256, 15>,
+                                                     kernel_config<256, 12>>>>
+{};
+
+// TODO: We need to update these parameters
+template<class Key, class Value>
+struct radix_sort_config_90a
+{
+    static constexpr unsigned int item_scale = ::rocprim::detail::ceiling_div<unsigned int>(
+        ::rocprim::max(sizeof(Key), sizeof(Value)), sizeof(int));
+
+    using scan = kernel_config<256, 1>;
+
+    using type = select_type<
+        select_type_case<
+            (sizeof(Key) == 1 && sizeof(Value) <= 8),
+            radix_sort_config<4, 4, scan, kernel_config<256, 5>, kernel_config<256, 19>>>,
+        select_type_case<
+            (sizeof(Key) == 2 && sizeof(Value) <= 8),
+            radix_sort_config<6, 5, scan, kernel_config<256, 5>, kernel_config<256, 17>>>,
+        select_type_case<
+            (sizeof(Key) == 4 && sizeof(Value) <= 8),
+            radix_sort_config<7, 6, scan, kernel_config<256, 7>, kernel_config<256, 15>>>,
+        select_type_case<
+            (sizeof(Key) == 8 && sizeof(Value) <= 8),
+            radix_sort_config<7, 6, scan, kernel_config<256, 7>, kernel_config<256, 14>>>,
+        radix_sort_config<
+            6,
+            4,
+            scan,
+            kernel_config<limit_block_size<256U, sizeof(Value), ROCPRIM_WARP_SIZE_64>::value,
+                          ::rocprim::max(1u, 15u / item_scale)>,
+            kernel_config<limit_block_size<256U, sizeof(Value), ROCPRIM_WARP_SIZE_64>::value,
+                          ::rocprim::max(1u, 10u / item_scale)>>>;
+};
+
+template<class Key>
+struct radix_sort_config_90a<Key, empty_type>
+    : select_type<select_type_case<sizeof(Key) == 1,
+                                   radix_sort_config<4,
+                                                     3,
+                                                     kernel_config<256, 1>,
+                                                     kernel_config<256, 5>,
+                                                     kernel_config<256, 19>>>,
+                  select_type_case<sizeof(Key) == 2,
+                                   radix_sort_config<6,
+                                                     5,
+                                                     kernel_config<256, 1>,
+                                                     kernel_config<256, 5>,
+                                                     kernel_config<256, 17>>>,
+                  select_type_case<sizeof(Key) == 4,
+                                   radix_sort_config<7,
+                                                     6,
+                                                     kernel_config<256, 1>,
+                                                     kernel_config<256, 8>,
+                                                     kernel_config<256, 15>>>,
+                  select_type_case<sizeof(Key) == 8,
+                                   radix_sort_config<7,
+                                                     6,
+                                                     kernel_config<256, 1>,
+                                                     kernel_config<256, 7>,
+                                                     kernel_config<256, 14>>>>
+{};
+
+// TODO: We need to update these parameters
+template<class Key, class Value>
+struct radix_sort_config_1030
+{
+    static constexpr unsigned int item_scale = ::rocprim::detail::ceiling_div<unsigned int>(
+        ::rocprim::max(sizeof(Key), sizeof(Value)), sizeof(int));
+
+    using scan = kernel_config<256, 2>;
+
+    using type = select_type<
+        select_type_case<
+            (sizeof(Key) == 1 && sizeof(Value) <= 8),
+            radix_sort_config<4, 4, scan, kernel_config<256, 10>, kernel_config<256, 19>>>,
+        select_type_case<
+            (sizeof(Key) == 2 && sizeof(Value) <= 8),
+            radix_sort_config<6, 5, scan, kernel_config<256, 10>, kernel_config<256, 17>>>,
+        select_type_case<
+            (sizeof(Key) == 4 && sizeof(Value) <= 8),
+            radix_sort_config<7, 6, scan, kernel_config<256, 15>, kernel_config<256, 15>>>,
+        select_type_case<
+            (sizeof(Key) == 8 && sizeof(Value) <= 8),
+            radix_sort_config<7, 6, scan, kernel_config<256, 15>, kernel_config<256, 14>>>,
+        radix_sort_config<
+            6,
+            4,
+            scan,
+            kernel_config<limit_block_size<256U, sizeof(Value), ROCPRIM_WARP_SIZE_32>::value,
+                          ::rocprim::max(1u, 15u / item_scale)>,
+            kernel_config<limit_block_size<256U, sizeof(Value), ROCPRIM_WARP_SIZE_64>::value,
+                          ::rocprim::max(1u, 10u / item_scale)>,
+            kernel_config<limit_block_size<256U, sizeof(Value), ROCPRIM_WARP_SIZE_64>::value,
+                          ::rocprim::max(1u, 10u / item_scale)>>>;
+};
+
+template<class Key>
+struct radix_sort_config_1030<Key, empty_type>
+    : select_type<select_type_case<sizeof(Key) == 1,
+                                   radix_sort_config<4,
+                                                     3,
+                                                     kernel_config<256, 2>,
+                                                     kernel_config<256, 10>,
+                                                     kernel_config<256, 19>>>,
+                  select_type_case<sizeof(Key) == 2,
+                                   radix_sort_config<6,
+                                                     5,
+                                                     kernel_config<256, 2>,
+                                                     kernel_config<256, 10>,
+                                                     kernel_config<256, 19>>>,
+                  select_type_case<sizeof(Key) == 4,
+                                   radix_sort_config<7,
+                                                     6,
+                                                     kernel_config<256, 2>,
+                                                     kernel_config<256, 17>,
+                                                     kernel_config<256, 17>>>,
+                  select_type_case<sizeof(Key) == 8,
+                                   radix_sort_config<7,
+                                                     6,
+                                                     kernel_config<256, 2>,
+                                                     kernel_config<256, 15>,
+                                                     kernel_config<256, 15>>>>
+{};
+
+template<unsigned int TargetArch, class Key, class Value>
+struct default_radix_sort_config
+    : select_arch<TargetArch,
+                  select_arch_case<803, radix_sort_config_803<Key, Value>>,
+                  select_arch_case<900, radix_sort_config_900<Key, Value>>,
+                  select_arch_case<908, radix_sort_config_908<Key, Value>>,
+                  select_arch_case<ROCPRIM_ARCH_90a, radix_sort_config_90a<Key, Value>>,
+                  select_arch_case<1030, radix_sort_config_1030<Key, Value>>,
+                  radix_sort_config_900<Key, Value>>
+{};
+
+} // end namespace detail
+
+END_ROCPRIM_NAMESPACE
+
+/// @}
+// end of group primitivesmodule_deviceconfigs
+
+#endif // ROCPRIM_DEVICE_DETAIL_CONFIG_DEVICE_RADIX_SORT_HPP_
---
+++ rocprim/include/rocprim/device/detail/config/device_scan_by_key_config.hpp
@@ -0,0 +1,125 @@
+// Copyright (c) 2022 Advanced Micro Devices, Inc. All rights reserved.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+#ifndef ROCPRIM_DEVICE_DETAIL_CONFIG_DEVICE_SCAN_BY_KEY_HPP_
+#define ROCPRIM_DEVICE_DETAIL_CONFIG_DEVICE_SCAN_BY_KEY_HPP_
+
+#include <type_traits>
+
+#include "../../../config.hpp"
+#include "../../../detail/various.hpp"
+#include "../../../functional.hpp"
+
+#include "../../config_types.hpp"
+#include "../device_config_helper.hpp"
+
+#include "../../../block/block_load.hpp"
+#include "../../../block/block_scan.hpp"
+#include "../../../block/block_store.hpp"
+
+#include "device_scan_config.hpp"
+
+/// \addtogroup primitivesmodule_deviceconfigs
+/// @{
+
+BEGIN_ROCPRIM_NAMESPACE
+
+namespace detail
+{
+
+template<class Key, class Value>
+struct scan_by_key_config_900
+{
+    static constexpr unsigned int item_scale = ::rocprim::detail::ceiling_div<unsigned int>(
+        sizeof(Key) + sizeof(Value), 2 * sizeof(int));
+
+    using type = scan_config<
+        limit_block_size<256U, sizeof(Key) + sizeof(Value), ROCPRIM_WARP_SIZE_64>::value,
+        ::rocprim::max(1u, 16u / item_scale),
+        ROCPRIM_DETAIL_USE_LOOKBACK_SCAN,
+        ::rocprim::block_load_method::block_load_transpose,
+        ::rocprim::block_store_method::block_store_transpose,
+        ::rocprim::block_scan_algorithm::using_warp_scan>;
+};
+
+template<class Key, class Value>
+struct scan_by_key_config_90a
+{
+    static constexpr unsigned int item_scale = ::rocprim::detail::ceiling_div<unsigned int>(
+        sizeof(Key) + sizeof(Value), 2 * sizeof(int));
+
+    using type = scan_config<
+        limit_block_size<256U, sizeof(Key) + sizeof(Value), ROCPRIM_WARP_SIZE_64>::value,
+        ::rocprim::max(1u, 16u / item_scale),
+        ROCPRIM_DETAIL_USE_LOOKBACK_SCAN,
+        ::rocprim::block_load_method::block_load_transpose,
+        ::rocprim::block_store_method::block_store_transpose,
+        ::rocprim::block_scan_algorithm::using_warp_scan>;
+};
+
+template<class Key, class Value>
+struct scan_by_key_config_908
+{
+    static constexpr unsigned int item_scale = ::rocprim::detail::ceiling_div<unsigned int>(
+        sizeof(Key) + sizeof(Value), 2 * sizeof(int));
+
+    using type = scan_config<
+        limit_block_size<256U, sizeof(Key) + sizeof(Value), ROCPRIM_WARP_SIZE_64>::value,
+        ::rocprim::max(1u, 20u / item_scale),
+        ROCPRIM_DETAIL_USE_LOOKBACK_SCAN,
+        ::rocprim::block_load_method::block_load_transpose,
+        ::rocprim::block_store_method::block_store_transpose,
+        ::rocprim::block_scan_algorithm::using_warp_scan>;
+};
+
+// TODO: We need to update these parameters
+template<class Key, class Value>
+struct scan_by_key_config_1030
+{
+    static constexpr unsigned int item_scale = ::rocprim::detail::ceiling_div<unsigned int>(
+        sizeof(Key) + sizeof(Value), 2 * sizeof(int));
+
+    using type = scan_config<
+        limit_block_size<256U, sizeof(Key) + sizeof(Value), ROCPRIM_WARP_SIZE_32>::value,
+        ::rocprim::max(1u, 15u / item_scale),
+        ROCPRIM_DETAIL_USE_LOOKBACK_SCAN,
+        ::rocprim::block_load_method::block_load_transpose,
+        ::rocprim::block_store_method::block_store_transpose,
+        ::rocprim::block_scan_algorithm::using_warp_scan>;
+};
+
+template<unsigned int TargetArch, class Key, class Value>
+struct default_scan_by_key_config
+    : select_arch<TargetArch,
+                  select_arch_case<900, scan_by_key_config_900<Key, Value>>,
+                  select_arch_case<ROCPRIM_ARCH_90a, scan_by_key_config_90a<Key, Value>>,
+                  select_arch_case<908, scan_by_key_config_908<Key, Value>>,
+                  select_arch_case<1030, scan_by_key_config_1030<Key, Value>>,
+                  scan_by_key_config_900<Key, Value>>
+{};
+
+} // end namespace detail
+
+END_ROCPRIM_NAMESPACE
+
+/// @}
+// end of group primitivesmodule_deviceconfigs
+
+#endif // ROCPRIM_DEVICE_DETAIL_CONFIG_DEVICE_SCAN_BY_KEY_HPP_
