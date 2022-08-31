3303c0de46f93b249b0d2b50a640f1ba078b0c20
Lauren Wrubleski <Lauren.Wrubleski@amd.com>
2022-08-31 09:54:40 -0600

    use echo instead of herestring

--- .jenkins/autotune.groovy
+++ .jenkins/autotune.groovy
@@ -23,7 +23,7 @@ def runCI =
 
         commonGroovy = load "${project.paths.project_src_prefix}/.jenkins/common.groovy"
         commonGroovy.runTestCommand(platform, project, auxiliary.gfxTargetParser() + 
-            '\n./autotune_usage_script.sh $(sed -E "s/gfx([0-9a-f]+).*/\\1/" <<< $gpu_arch)')
+            '\n./autotune_usage_script.sh $(echo "$gpu_arch" | sed -E "s/gfx([0-9a-f]+).*/\\1/")')
     }
 
 
