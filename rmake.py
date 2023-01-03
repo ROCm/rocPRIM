49e8d5f4b13198dac7349e2650addf4b481160fe
Stanley Tsang <stanley.tsang@amd.com>
2023-01-03 15:26:06 -0700

    Updating copyright year

--- rmake.py
+++ rmake.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Copyright (c) 2021-2022 Advanced Micro Devices, Inc. All rights reserved.
+""" Copyright (c) 2021-2023 Advanced Micro Devices, Inc. All rights reserved.
 Manage build and installation"""
 
 import re
