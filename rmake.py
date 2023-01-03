cb0eafcc297bfd17fa991fe80edf99040f16fae1
Stanley Tsang <stanley.tsang@amd.com>
2023-01-03 14:35:12 -0700

    Fix typo

--- rmake.py
+++ rmake.py
@@ -76,7 +76,7 @@ def delete_dir(dir_path) :
         run_cmd( "rm" , f"-rf {linux_path}")
 
 def cmake_path(os_path):
-    if OS_info == "nt":
+    if OS_info["ID"] == "windows":
         return os_path.replace("\\", "/")
     else:
         return os.path.realpath(os_path)     
