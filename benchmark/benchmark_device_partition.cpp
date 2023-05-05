82f814cacb93923ab58bb4d3a509ea5aeb8d833c
Nara Prasetya <nara@streamhpc.com>
2023-05-05 13:29:02 +0000

    feat(device_partition): added partition_two_way

--- benchmark/benchmark_device_partition.cpp
+++ benchmark/benchmark_device_partition.cpp
@@ -1,6 +1,6 @@
 // MIT License
 //
-// Copyright (c) 2017-2022 Advanced Micro Devices, Inc. All rights reserved.
+// Copyright (c) 2017-2023 Advanced Micro Devices, Inc. All rights reserved.
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -287,6 +287,106 @@ void run_if_benchmark(benchmark::State& state,
 }
 
 template<class T>
+void run_two_way_benchmark(benchmark::State& state,
+                           size_t            size,
+                           const hipStream_t stream,
+                           float             probability)
+{
+    auto predicate
+        = [probability] __device__(const T& value) { return value < T(127 * probability); };
+
+    std::vector<T> input = get_random_data<T>(size, T(0), T(127));
+    T*             d_input;
+    T*             d_output_selected;
+    T*             d_output_rejected;
+    unsigned int*  d_selected_count_output;
+    HIP_CHECK(hipMalloc(&d_input, input.size() * sizeof(T)));
+    HIP_CHECK(hipMalloc(&d_output_selected, input.size() * sizeof(T)));
+    HIP_CHECK(hipMalloc(&d_output_rejected, input.size() * sizeof(T)));
+    HIP_CHECK(hipMalloc(&d_selected_count_output, sizeof(*d_selected_count_output)));
+    HIP_CHECK(hipMemcpy(d_input, input.data(), input.size() * sizeof(T), hipMemcpyHostToDevice));
+    HIP_CHECK(hipDeviceSynchronize());
+    // Allocate temporary storage memory
+    size_t temp_storage_size_bytes;
+
+    // Get size of d_temp_storage
+    HIP_CHECK(rocprim::partition_two_way(nullptr,
+                                         temp_storage_size_bytes,
+                                         d_input,
+                                         d_output_selected,
+                                         d_output_rejected,
+                                         d_selected_count_output,
+                                         input.size(),
+                                         predicate,
+                                         stream));
+
+    // allocate temporary storage
+    void* d_temp_storage = nullptr;
+    HIP_CHECK(hipMalloc(&d_temp_storage, temp_storage_size_bytes));
+
+    // Warm-up
+    for(size_t i = 0; i < 10; i++)
+    {
+        HIP_CHECK(rocprim::partition_two_way(d_temp_storage,
+                                             temp_storage_size_bytes,
+                                             d_input,
+                                             d_output_selected,
+                                             d_output_rejected,
+                                             d_selected_count_output,
+                                             input.size(),
+                                             predicate,
+                                             stream));
+    }
+    HIP_CHECK(hipDeviceSynchronize());
+
+    // HIP events creation
+    hipEvent_t start, stop;
+    HIP_CHECK(hipEventCreate(&start));
+    HIP_CHECK(hipEventCreate(&stop));
+
+    const unsigned int batch_size = 10;
+    for(auto _ : state)
+    {
+        // Record start event
+        HIP_CHECK(hipEventRecord(start, stream));
+
+        for(size_t i = 0; i < batch_size; i++)
+        {
+            HIP_CHECK(rocprim::partition_two_way(d_temp_storage,
+                                                 temp_storage_size_bytes,
+                                                 d_input,
+                                                 d_output_selected,
+                                                 d_output_rejected,
+                                                 d_selected_count_output,
+                                                 input.size(),
+                                                 predicate,
+                                                 stream));
+        }
+
+        // Record stop event and wait until it completes
+        HIP_CHECK(hipEventRecord(stop, stream));
+        HIP_CHECK(hipEventSynchronize(stop));
+
+        float elapsed_mseconds;
+        HIP_CHECK(hipEventElapsedTime(&elapsed_mseconds, start, stop));
+        state.SetIterationTime(elapsed_mseconds / 1000);
+    }
+
+    // Destroy HIP events
+    HIP_CHECK(hipEventDestroy(start));
+    HIP_CHECK(hipEventDestroy(stop));
+
+    state.SetBytesProcessed(state.iterations() * batch_size * size * sizeof(T));
+    state.SetItemsProcessed(state.iterations() * batch_size * size);
+
+    hipFree(d_input);
+    hipFree(d_output_selected);
+    hipFree(d_output_rejected);
+    hipFree(d_selected_count_output);
+    hipFree(d_temp_storage);
+}
+
+template<class T>
 void run_three_way_benchmark(benchmark::State& state,
                              size_t size,
                              const hipStream_t stream,
@@ -437,6 +537,16 @@ void run_three_way_benchmark(benchmark::State& state,
         stream,                                                                         \
         p)
 
+#define CREATE_PARTITION_TWO_WAY_BENCHMARK(T, p)                                             \
+    benchmark::RegisterBenchmark(                                                            \
+        bench_naming::format_name("{lvl:device,algo:partition,key_type:" #T                  \
+                                  ",subalgo:two_way,probability:" #p ",cfg:default_config}") \
+            .c_str(),                                                                        \
+        run_two_way_benchmark<T>,                                                            \
+        size,                                                                                \
+        stream,                                                                              \
+        p)
+
 #define CREATE_PARTITION_THREE_WAY_BENCHMARK(T, p1, p2)                                       \
     benchmark::RegisterBenchmark(                                                             \
         bench_naming::format_name("{lvl:device,algo:partition,key_type:" #T                   \
@@ -448,6 +558,8 @@ void run_three_way_benchmark(benchmark::State& state,
         stream,                                                                               \
         p1,                                                                                   \
         p2)
+
+// clang-format off
 
 #define BENCHMARK_FLAGGED_TYPE(type, value) \
     CREATE_PARTITION_FLAGGED_BENCHMARK(type, value, 0.05f), \
@@ -461,11 +573,19 @@ void run_three_way_benchmark(benchmark::State& state,
     CREATE_PARTITION_IF_BENCHMARK(type, 0.5f), \
     CREATE_PARTITION_IF_BENCHMARK(type, 0.75f)
 
+#define BENCHMARK_TWO_WAY_TYPE(type) \
+    CREATE_PARTITION_TWO_WAY_BENCHMARK(type, 0.05f), \
+    CREATE_PARTITION_TWO_WAY_BENCHMARK(type, 0.25f), \
+    CREATE_PARTITION_TWO_WAY_BENCHMARK(type, 0.5f), \
+    CREATE_PARTITION_TWO_WAY_BENCHMARK(type, 0.75f)
+
 #define BENCHMARK_THREE_WAY_TYPE(type) \
     CREATE_PARTITION_THREE_WAY_BENCHMARK(type, 0.05f, 0.25f), \
     CREATE_PARTITION_THREE_WAY_BENCHMARK(type, 0.25f, 0.5f), \
     CREATE_PARTITION_THREE_WAY_BENCHMARK(type, 0.5f, 0.75f), \
     CREATE_PARTITION_THREE_WAY_BENCHMARK(type, 0.75f, 1.f)
+
+// clang-format on
 
 int main(int argc, char *argv[])
 {
@@ -495,32 +615,38 @@ int main(int argc, char *argv[])
     using custom_int_double = custom_type<int, double>;
 
     // Add benchmarks
-    std::vector<benchmark::internal::Benchmark*> benchmarks =
-    {
-        BENCHMARK_FLAGGED_TYPE(int, unsigned char),
-        BENCHMARK_FLAGGED_TYPE(float, unsigned char),
-        BENCHMARK_FLAGGED_TYPE(double, unsigned char),
-        BENCHMARK_FLAGGED_TYPE(uint8_t, uint8_t),
-        BENCHMARK_FLAGGED_TYPE(int8_t, int8_t),
-        BENCHMARK_FLAGGED_TYPE(rocprim::half, int8_t),
-        BENCHMARK_FLAGGED_TYPE(custom_double2, unsigned char),
+    std::vector<benchmark::internal::Benchmark*> benchmarks
+        = {BENCHMARK_FLAGGED_TYPE(int, unsigned char),
+           BENCHMARK_FLAGGED_TYPE(float, unsigned char),
+           BENCHMARK_FLAGGED_TYPE(double, unsigned char),
+           BENCHMARK_FLAGGED_TYPE(uint8_t, uint8_t),
+           BENCHMARK_FLAGGED_TYPE(int8_t, int8_t),
+           BENCHMARK_FLAGGED_TYPE(rocprim::half, int8_t),
+           BENCHMARK_FLAGGED_TYPE(custom_double2, unsigned char),
 
-        BENCHMARK_IF_TYPE(int),
-        BENCHMARK_IF_TYPE(float),
-        BENCHMARK_IF_TYPE(double),
-        BENCHMARK_IF_TYPE(uint8_t),
-        BENCHMARK_IF_TYPE(int8_t),
-        BENCHMARK_IF_TYPE(rocprim::half),
-        BENCHMARK_IF_TYPE(custom_int_double),
+           BENCHMARK_IF_TYPE(int),
+           BENCHMARK_IF_TYPE(float),
+           BENCHMARK_IF_TYPE(double),
+           BENCHMARK_IF_TYPE(uint8_t),
+           BENCHMARK_IF_TYPE(int8_t),
+           BENCHMARK_IF_TYPE(rocprim::half),
+           BENCHMARK_IF_TYPE(custom_int_double),
 
-        BENCHMARK_THREE_WAY_TYPE(int),
-        BENCHMARK_THREE_WAY_TYPE(float),
-        BENCHMARK_THREE_WAY_TYPE(double),
-        BENCHMARK_THREE_WAY_TYPE(uint8_t),
-        BENCHMARK_THREE_WAY_TYPE(int8_t),
-        BENCHMARK_THREE_WAY_TYPE(rocprim::half),
-        BENCHMARK_THREE_WAY_TYPE(custom_int_double)
-    };
+           BENCHMARK_TWO_WAY_TYPE(int),
+           BENCHMARK_TWO_WAY_TYPE(float),
+           BENCHMARK_TWO_WAY_TYPE(double),
+           BENCHMARK_TWO_WAY_TYPE(uint8_t),
+           BENCHMARK_TWO_WAY_TYPE(int8_t),
+           BENCHMARK_TWO_WAY_TYPE(rocprim::half),
+           BENCHMARK_TWO_WAY_TYPE(custom_int_double),
+
+           BENCHMARK_THREE_WAY_TYPE(int),
+           BENCHMARK_THREE_WAY_TYPE(float),
+           BENCHMARK_THREE_WAY_TYPE(double),
+           BENCHMARK_THREE_WAY_TYPE(uint8_t),
+           BENCHMARK_THREE_WAY_TYPE(int8_t),
+           BENCHMARK_THREE_WAY_TYPE(rocprim::half),
+           BENCHMARK_THREE_WAY_TYPE(custom_int_double)};
 
     // Use manual timing
     for(auto& b : benchmarks)
