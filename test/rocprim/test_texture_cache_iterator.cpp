7129057e73135e1e18da448967759c5477cbfa72
Stanley Tsang <stanley.tsang@amd.com>
2023-08-11 13:08:24 -0600

    Do not compile texture cache functions for gfx94x (#462)
    * Do not compile texture cache test for gfx94x
    
    * Add newline back to EOF
    
    * Update documentation to explain texture cache iterator is not supported for gfx94x
    
    * Disable/add warnings to texture cache iterator for gfx94x.  Remove test warnings
    
    * undo comment
    
    * Add documentation
    
    [ROCm/rocPRIM commit: cca56ad007b694048d402c291eede982fb30401d]

--- test/rocprim/test_texture_cache_iterator.cpp
+++ test/rocprim/test_texture_cache_iterator.cpp
@@ -76,8 +76,7 @@ TYPED_TEST(RocprimTextureCacheIteratorTests, Transform)
     std::string deviceName = std::string(props.gcnArchName);
     if (deviceName.rfind("gfx94", 0) == 0) {
         // This is a gfx94x device, so skip this test
-        SCOPED_TRACE(testing::Message() << "Skipping texture cache text for " << deviceName);
-        return;
+        GTEST_SKIP() << "Test not run on gfx94x as texture cache API is not supported";
     }
 
     HIP_CHECK(hipSetDevice(device_id));
