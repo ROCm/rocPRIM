9c0ca70f86ea9acbaa109ae003a1adc543ec2e75
Lőrinc Serfőző <lorinc@streamhpc.com>
2024-02-07 12:18:51 +0000

    Implemented sort_comparator for custom test type

--- test/rocprim/test_utils_sort_comparator.hpp
+++ test/rocprim/test_utils_sort_comparator.hpp
@@ -1,6 +1,6 @@
 // MIT License
 //
-// Copyright (c) 2017-2023 Advanced Micro Devices, Inc. All rights reserved.
+// Copyright (c) 2017-2024 Advanced Micro Devices, Inc. All rights reserved.
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -25,11 +25,89 @@
 
 #include <rocprim/type_traits.hpp>
 
-#include "test_utils_half.hpp"
+#include "rocprim/detail/radix_sort.hpp"
 #include "test_utils_bfloat16.hpp"
+#include "test_utils_custom_test_types.hpp"
+#include "test_utils_half.hpp"
+
+#include <type_traits>
 
 namespace test_utils
 {
+namespace detail
+{
+
+template<unsigned int StartBit,
+         unsigned int EndBit,
+         class Key,
+         std::enable_if_t<rocprim::is_integral<Key>::value || std::is_same<Key, __uint128_t>::value
+                              || std::is_same<Key, __int128_t>::value,
+                          int>
+         = 0>
+Key to_bits(const Key key)
+{
+    static constexpr Key radix_mask_upper
+        = EndBit == 8 * sizeof(Key) ? ~Key(0) : static_cast<Key>((Key(1) << EndBit) - 1);
+    static constexpr Key radix_mask_bottom = static_cast<Key>((Key(1) << StartBit) - 1);
+    static constexpr Key radix_mask        = radix_mask_upper ^ radix_mask_bottom;
+
+    return key & radix_mask;
+}
+
+template<unsigned int StartBit,
+         unsigned int EndBit,
+         class Key,
+         std::enable_if_t<rocprim::is_floating_point<Key>::value, int> = 0>
+auto to_bits(const Key key)
+{
+    using unsigned_bits_type = typename rocprim::get_unsigned_bits_type<Key>::unsigned_type;
+
+    unsigned_bits_type bit_key;
+    memcpy(&bit_key, &key, sizeof(Key));
+
+    // Remove signed zero, this case is supposed to be treated the same as
+    // unsigned zero in rocprim sorting algorithms.
+    constexpr unsigned_bits_type minus_zero = unsigned_bits_type{1} << (8 * sizeof(Key) - 1);
+    // Positive and negative zero should compare the same.
+    if(bit_key == minus_zero)
+    {
+        bit_key = 0;
+    }
+    // Flip bits mantissa and exponent if the key is negative, so as to make
+    // 'more negative' values compare before 'less negative'.
+    if(bit_key & minus_zero)
+    {
+        bit_key ^= ~minus_zero;
+    }
+    // Make negatives compare before positives.
+    bit_key ^= minus_zero;
+
+    return to_bits<StartBit, EndBit>(bit_key);
+}
+
+template<unsigned int StartBit,
+         unsigned int EndBit,
+         class Key,
+         std::enable_if_t<is_custom_test_type<Key>::value, int> = 0>
+auto to_bits(const Key& key)
+{
+    using inner_t = typename inner_type<Key>::type;
+    // For two doubles, we need uint128, but that is not part of rocprim::get_unsigned_bits_type
+    using unsigned_bits_type = std::conditional_t<
+        sizeof(inner_t) == 8,
+        __uint128_t,
+        typename rocprim::get_unsigned_bits_type<void, rocprim::min(8ul, sizeof(inner_t) * 2)>::
+            unsigned_type>;
+
+    const unsigned_bits_type bit_key_upper
+        = static_cast<unsigned_bits_type>(to_bits<0, sizeof(key.x) * 8>(key.x))
+          << (8 * sizeof(inner_t));
+    const unsigned_bits_type bit_key_lower = to_bits<0, sizeof(key.x) * 8>(key.y);
+
+    return to_bits<StartBit, EndBit>(bit_key_upper | bit_key_lower);
+}
+
+} // namespace detail
 
 template<class T>
 constexpr bool is_floating_nan_host(const T& a)
@@ -37,112 +115,14 @@ constexpr bool is_floating_nan_host(const T& a)
     return (a != a);
 }
 
-template<class Key,
-         bool         Descending,
-         unsigned int StartBit,
-         unsigned int EndBit,
-         class Enable = void>
+template<class Key, bool Descending, unsigned int StartBit, unsigned int EndBit>
 struct key_comparator
-{};
-
-template<class Key, bool Descending, unsigned int StartBit, unsigned int EndBit>
-struct key_comparator<Key,
-                      Descending,
-                      StartBit,
-                      EndBit,
-                      typename std::enable_if<rocprim::is_integral<Key>::value>::type>
 {
-    static constexpr Key radix_mask_upper
-        = EndBit == 8 * sizeof(Key) ? ~Key(0) : (Key(1) << EndBit) - 1;
-    static constexpr Key radix_mask_bottom = (Key(1) << StartBit) - 1;
-    static constexpr Key radix_mask = radix_mask_upper ^ radix_mask_bottom;
-
-    bool operator()(const Key& lhs, const Key& rhs) const
+    bool operator()(const Key lhs, const Key rhs) const
     {
-        Key l = lhs & radix_mask;
-        Key r = rhs & radix_mask;
+        const auto l = detail::to_bits<StartBit, EndBit>(lhs);
+        const auto r = detail::to_bits<StartBit, EndBit>(rhs);
         return Descending ? (r < l) : (l < r);
-    }
-};
-
-template<class Key, bool Descending, unsigned int StartBit, unsigned int EndBit>
-struct key_comparator<Key,
-                      Descending,
-                      StartBit,
-                      EndBit,
-                      typename std::enable_if<std::is_same<Key, __int128_t>::value>::type>
-{
-    static constexpr Key radix_mask_upper
-        = EndBit == 8 * sizeof(Key) ? ~Key(0) : (Key(1) << EndBit) - 1;
-    static constexpr Key radix_mask_bottom = (Key(1) << StartBit) - 1;
-    static constexpr Key radix_mask        = radix_mask_upper ^ radix_mask_bottom;
-
-    bool operator()(const Key& lhs, const Key& rhs) const
-    {
-        Key l = lhs & radix_mask;
-        Key r = rhs & radix_mask;
-        return Descending ? (r < l) : (l < r);
-    }
-};
-
-template<class Key, bool Descending, unsigned int StartBit, unsigned int EndBit>
-struct key_comparator<Key,
-                      Descending,
-                      StartBit,
-                      EndBit,
-                      typename std::enable_if<std::is_same<Key, __uint128_t>::value>::type>
-{
-    static constexpr Key radix_mask_upper
-        = EndBit == 8 * sizeof(Key) ? ~Key(0) : (Key(1) << EndBit) - 1;
-    static constexpr Key radix_mask_bottom = (Key(1) << StartBit) - 1;
-    static constexpr Key radix_mask        = radix_mask_upper ^ radix_mask_bottom;
-
-    bool operator()(const Key& lhs, const Key& rhs) const
-    {
-        Key l = lhs & radix_mask;
-        Key r = rhs & radix_mask;
-        return Descending ? (r < l) : (l < r);
-    }
-};
-
-template<class Key, bool Descending, unsigned int StartBit, unsigned int EndBit>
-struct key_comparator<Key,
-                      Descending,
-                      StartBit,
-                      EndBit,
-                      typename std::enable_if<rocprim::is_floating_point<Key>::value>::type>
-{
-    using unsigned_bits_type = typename rocprim::get_unsigned_bits_type<Key>::unsigned_type;
-
-    bool operator()(const Key& lhs, const Key& rhs) const
-    {
-        return key_comparator<unsigned_bits_type, Descending, StartBit, EndBit>()(
-            this->to_bits(lhs),
-            this->to_bits(rhs));
-    }
-
-    unsigned_bits_type to_bits(const Key& key) const
-    {
-        unsigned_bits_type bit_key;
-        memcpy(&bit_key, &key, sizeof(Key));
-
-        // Remove signed zero, this case is supposed to be treated the same as
-        // unsigned zero in rocprim sorting algorithms.
-        constexpr unsigned_bits_type minus_zero = unsigned_bits_type{1} << (8 * sizeof(Key) - 1);
-        // Positive and negative zero should compare the same.
-        if(bit_key == minus_zero)
-        {
-            bit_key = 0;
-        }
-        // Flip bits mantissa and exponent if the key is negative, so as to make
-        // 'more negative' values compare before 'less negative'.
-        if(bit_key & minus_zero)
-        {
-            bit_key ^= ~minus_zero;
-        }
-        // Make negatives compare before positives.
-        bit_key ^= minus_zero;
-        return bit_key;
     }
 };
 
@@ -155,25 +135,34 @@ struct key_value_comparator
     }
 };
 
-template <bool Descending>
-struct key_comparator<rocprim::half, Descending, 0, sizeof(rocprim::half) * 8>
+template<class CustomTestType>
+struct custom_test_type_decomposer
 {
-    bool operator()(const rocprim::half& lhs, const rocprim::half& rhs)
+    static_assert(is_custom_test_type<CustomTestType>::value,
+                  "custom_test_type_decomposer can only be used with custom_test_type<T>");
+    using inner_t = typename inner_type<CustomTestType>::type;
+
+    __host__ __device__ auto operator()(CustomTestType& key) const
     {
-        // HIP's half doesn't have __host__ comparison operators, use floats instead
-        return key_comparator<float, Descending, 0, sizeof(float) * 8>()(lhs, rhs);
+        return ::rocprim::tuple<inner_t&, inner_t&>{key.x, key.y};
     }
 };
 
-template <bool Descending>
-struct key_comparator<rocprim::bfloat16, Descending, 0, sizeof(rocprim::bfloat16) * 8>
+template<class Key>
+struct select_decomposer
 {
-    bool operator()(const rocprim::bfloat16& lhs, const rocprim::bfloat16& rhs)
-    {
-        // HIP's bfloat16 doesn't have __host__ comparison operators, use floats instead
-        return key_comparator<float, Descending, 0, sizeof(float) * 8>()(lhs, rhs);
-    }
+    using type = ::rocprim::detail::identity_decomposer;
 };
 
-}
+template<class InnerType>
+struct select_decomposer<custom_test_type<InnerType>>
+{
+    using type = custom_test_type_decomposer<custom_test_type<InnerType>>;
+};
+
+template<class Key>
+using select_decomposer_t = typename select_decomposer<Key>::type;
+
+} // namespace test_utils
+
 #endif // TEST_UTILS_SORT_COMPARATOR_HPP_
