dd41a05a0aeaed83c2b722c2ae5ca37426edb33c
NguyenNhuDi <dinguyennhu@gmail.com>
2024-07-10 15:06:26 -0600

    tuned up block run length decode and config dispatch

--- benchmark/benchmark_config_dispatch.cpp
+++ benchmark/benchmark_config_dispatch.cpp
@@ -95,15 +95,13 @@ int main(int argc, char** argv)
     const int    trials = parser.get<int>("trials");
     bench_naming::set_format(parser.get<std::string>("name_format"));
 
-    std::cout << "benchmark_config_dispatch" << std::endl;
 
     // HIP
-    hipStream_t     stream = 0; // default
-    hipDeviceProp_t devProp;
-    int             device_id = 0;
-    HIP_CHECK(hipGetDevice(&device_id));
-    HIP_CHECK(hipGetDeviceProperties(&devProp, device_id));
-    std::cout << "[HIP] Device name: " << devProp.name << std::endl;
+    // hipStream_t     stream = 0; // default
+    // hipDeviceProp_t devProp;
+    // int             device_id = 0;
+    // HIP_CHECK(hipGetDevice(&device_id));
+    // HIP_CHECK(hipGetDeviceProperties(&devProp, device_id));
 
     std::vector<benchmark::internal::Benchmark*> benchmarks{
         CREATE_BENCHMARK(default_stream, stream_kind::default_stream),
