45b19428461d8860766d5c30fc8a2bfefb26c2a7
Nick Breed <78807921+NB4444@users.noreply.github.com>
2024-09-17 05:40:43 +0200

    Add new generate_limit tool to fix numeric_limits not working on custom types and rocprim::half (#604)

--- benchmark/benchmark_device_memory.cpp
+++ benchmark/benchmark_device_memory.cpp
@@ -335,19 +335,12 @@ void run_benchmark(benchmark::State&   state,
                    const managed_seed& seed,
                    const hipStream_t   stream)
 {
-    const size_t grid_size = size / (BlockSize * ItemsPerThread);
-    std::vector<T> input;
-    if(std::is_floating_point<T>::value)
-    {
-        input = get_random_data<T>(size, static_cast<T>(-1000), static_cast<T>(1000), seed.get_0());
-    }
-    else
-    {
-        input = get_random_data<T>(size,
-                                   std::numeric_limits<T>::min(),
-                                   std::numeric_limits<T>::max(),
-                                   seed.get_0());
-    }
+    const size_t   grid_size = size / (BlockSize * ItemsPerThread);
+    std::vector<T> input     = get_random_data<T>(size,
+                                              generate_limits<T>::min(),
+                                              generate_limits<T>::max(),
+                                              seed.get_0());
+
     T * d_input;
     T * d_output;
     HIP_CHECK(hipMalloc(reinterpret_cast<void**>(&d_input), size * sizeof(T)));
