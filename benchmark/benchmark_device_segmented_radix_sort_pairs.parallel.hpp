951662b0d2a7fe74e94c5e1d5c07a9086382ee8f
Nara Prasetya <nara@streamhpc.com>
2024-09-26 07:57:22 +0000

    fix(benchmark_device_segmented_radix_sort_pairs): fix  really stupid compile issue

--- benchmark/benchmark_device_segmented_radix_sort_pairs.parallel.hpp
+++ benchmark/benchmark_device_segmented_radix_sort_pairs.parallel.hpp
@@ -131,9 +131,9 @@ struct device_segmented_radix_sort_benchmark : public config_autotune_interface
                                         seed.get_0());
 
         std::vector<value_type> values_input
-            = get_random_data<key_type>(size,
-                                        generate_limits<key_type>::min(),
-                                        generate_limits<key_type>::max(),
+            = get_random_data<value_type>(size,
+                                        generate_limits<value_type>::min(),
+                                        generate_limits<value_type>::max(),
                                         seed.get_0());
 
         size_t batch_size = 1;
