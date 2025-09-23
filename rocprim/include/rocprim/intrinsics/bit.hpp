9c1edf5557d3aec14e79b6c19fef11f8e956c8cd
amd-hsong <hao.song@amd.com>
2025-09-23 14:02:06 -0600

    [rocprim] Fix undefined behavior in rocprim/intrinsics/bit.hpp (#1565)
    ## Motivation
    
    <!-- Explain the purpose of this PR and the goals it aims to achieve.
    -->
    This PR adds the handling of undefined behavior in a few places where we
    call `__builtin_clz`, `__builtin_ctz`, etc.
    
    ## Technical Details
    
    <!-- Explain the changes along with any relevant GitHub links. -->
    The family of compiler intrinsics `__builtin_clz` and `__builtin_ctz`
    can give undefined behavior when the input argument is zero - we need to
    handle this case.
    
    ## Test Plan
    
    <!-- Explain any relevant testing done to verify this PR. -->
    Existing tests should all pass. The test failure caught by CI on
    `test_device_merge_inplace` should be fixed by this PR.
    
    ## Test Result
    
    <!-- Briefly summarize test outcomes. -->
    All rocprim tests pass, including `test_device_merge_inplace`.
    
    ## Submission Checklist
    
    - [ ] Look over the contributing guidelines at
    https://github.com/ROCm/ROCm/blob/develop/CONTRIBUTING.md#pull-requests.

--- rocprim/include/rocprim/intrinsics/bit.hpp
+++ rocprim/include/rocprim/intrinsics/bit.hpp
@@ -58,48 +58,53 @@ unsigned int bit_count(unsigned long long x)
 /// \brief Count trailing zeroes
 ///
 /// Count the number of consecutive 0-bits, starting from the
-/// least significant bit.
+/// least significant bit. \p x must be non-zero.
 ROCPRIM_HOST_DEVICE ROCPRIM_INLINE unsigned int ctz(unsigned int x)
 {
+    __hip_assert(x != 0 && "'x' should be non-zero!");
     return __builtin_ctz(x);
 }
 
 /// \brief Count trailing zeroes
 ///
 /// Count the number of consecutive 0-bits, starting from the
-/// least significant bit.
+/// least significant bit. \p x must be non-zero.
 ROCPRIM_HOST_DEVICE ROCPRIM_INLINE unsigned int ctz(unsigned long long x)
 {
+    __hip_assert(x != 0 && "'x' should be non-zero!");
     return __builtin_ctzll(x);
 }
 
 /// \brief Count leading zeroes
 ///
 /// Count the number of consecutive 0-bits, starting from the
-/// most significant bit.
+/// most significant bit. \p x must be non-zero.
 ROCPRIM_HOST_DEVICE ROCPRIM_INLINE
 int clz(unsigned int x)
 {
+    __hip_assert(x != 0 && "'x' should be non-zero!");
     return __builtin_clz(x);
 }
 
 /// \brief Count leading zeroes
 ///
 /// Count the number of consecutive 0-bits, starting from the
-/// most significant bit.
+/// most significant bit. \p x must be non-zero.
 ROCPRIM_HOST_DEVICE ROCPRIM_INLINE
 int clz(unsigned long x)
 {
+    __hip_assert(x != 0 && "'x' should be non-zero!");
     return __builtin_clzl(x);
 }
 
 /// \brief Count leading zeroes
 ///
 /// Count the number of consecutive 0-bits, starting from the
-/// most significant bit.
+/// most significant bit. \p x must be non-zero.
 ROCPRIM_HOST_DEVICE ROCPRIM_INLINE
 int clz(unsigned long long x)
 {
+    __hip_assert(x != 0 && "'x' should be non-zero!");
     return __builtin_clzll(x);
 }
 
