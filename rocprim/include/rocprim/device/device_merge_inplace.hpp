f777cc2f62b5f4e4a0490f9312ed3c5a08b25290
amd-hsong <220770958+amd-hsong@users.noreply.github.com>
2025-09-23 20:02:49 +0000

    [rocm-libraries] ROCm/rocm-libraries#1565 (commit a3dedcb)
    [rocprim] Fix undefined behavior in
     rocprim/intrinsics/bit.hpp (#1565)
    
    ## Motivation
    
    <!-- Explain the purpose of this PR and the goals it aims to achieve.
    -->
    This PR adds the handling of undefined behavior in a few places where we
    call `__builtin_clz`, `__builtin_ctz`, etc.
    
    ## Technical Details
    
    <!-- Explain the changes along with any relevant GitHub links. -->
    The family of compiler intrinsics `__builtin_clz` and `__builtin_ctz`
    can give undefined behavior when the input argument is zero - we need to
    handle this case.
    
    ## Test Plan
    
    <!-- Explain any relevant testing done to verify this PR. -->
    Existing tests should all pass. The test failure caught by CI on
    `test_device_merge_inplace` should be fixed by this PR.
    
    ## Test Result
    
    <!-- Briefly summarize test outcomes. -->
    All rocprim tests pass, including `test_device_merge_inplace`.
    
    ## Submission Checklist
    
    - [ ] Look over the contributing guidelines at
    https://github.com/ROCm/ROCm/blob/develop/CONTRIBUTING.md#pull-requests.

--- rocprim/include/rocprim/device/device_merge_inplace.hpp
+++ rocprim/include/rocprim/device/device_merge_inplace.hpp
@@ -262,7 +262,8 @@ struct merge_inplace_impl
     static auto get_num_global_divisions(size_t left_size, size_t right_size)
     {
         const offset_t max_size = max(left_size, right_size);
-        const int32_t  set_bits = std::numeric_limits<size_t>::digits - clz(max_size);
+        const int32_t  set_bits = max_size == 0 ? std::numeric_limits<size_t>::digits :
+                                  std::numeric_limits<size_t>::digits - clz(max_size);
 
         // compute 2 + ceil(log_2(max(left, right))) - log_2(items_per_thread)
         return max(2, 2 + set_bits - Log2<block_merge_items_per_block>::VALUE);
