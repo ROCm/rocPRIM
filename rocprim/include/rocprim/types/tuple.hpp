12e75cb3c467acc8d1387fac3b1f5faad88e0fde
Nara <nara@streamhpc.com>
2025-09-24 06:54:26 +0200

    feat(hipcub,rocprim): add 'rocprim::apply', 'hipcub::DeviceTransform', and bump cccl version (#1059)
    - Add `rocprim::apply`. This is required to handle tuple constructs in
    hipCUB.
    - Add `hipcub::DeviceTransform`.
    - Add a naive version of `TransformStableArgumentAddresses`.
    - Add two tests for stable argument addresses: 1) check if it works with
    non-copyable classes and 2) compute the diff between pointers.
    - Bumps required CCCL version to 2.8.2 for CUDA backend.
    
    Required for CCCL 2.8.x compatibility.

--- rocprim/include/rocprim/types/tuple.hpp
+++ rocprim/include/rocprim/types/tuple.hpp
@@ -21,8 +21,10 @@
 #ifndef ROCPRIM_TYPES_TUPLE_HPP_
 #define ROCPRIM_TYPES_TUPLE_HPP_
 
+#include <functional>
 #include <tuple>
 #include <type_traits>
+#include <utility>
 
 #include "../config.hpp"
 #include "../detail/all_true.hpp"
@@ -971,6 +973,15 @@ tuple_element_t<I, tuple<Types...>>&& get(tuple<Types...>&& t) noexcept
     return static_cast<value_type&&>(static_cast<type&>(t.base).get());
 }
 
+template<size_t I, class... Types>
+ROCPRIM_HOST_DEVICE
+inline tuple_element_t<I, tuple<Types...>>&& get(const tuple<Types...>&& t) noexcept
+{
+    using value_type = tuple_element_t<I, tuple<Types...>>;
+    using type       = detail::tuple_value<I, tuple_element_t<I, tuple<Types...>>>;
+    return static_cast<const value_type&&>(static_cast<const type&>(t.base).get());
+}
+
 // ////////////////////////
 // make_tuple
 // ////////////////////////
@@ -1069,6 +1080,32 @@ ROCPRIM_HOST_DEVICE inline
 tuple<Types&...> tie(Types&... args) noexcept
 {
     return ::rocprim::tuple<Types&...>(args...);
+}
+
+namespace detail
+{
+template<class Func, class Tuple, size_t... Indices>
+ROCPRIM_HOST_DEVICE ROCPRIM_INLINE
+auto apply_tuple(Func&& func, Tuple&& tuple, std::index_sequence<Indices...> /* indices */) noexcept
+{
+    return func(::rocprim::get<Indices>(std::forward<Tuple>(tuple))...);
+}
+} // namespace detail
+
+/// \brief Applies a function to a tuple.
+///
+/// \param func the function to apply.
+/// \param tuple the target tuple.
+///
+/// \see std::apply
+template<class Func, class Tuple>
+ROCPRIM_HOST_DEVICE ROCPRIM_INLINE
+auto apply(Func&& func, Tuple&& tuple) noexcept
+{
+    return ::rocprim::detail::apply_tuple(
+        std::forward<Func>(func),
+        std::forward<Tuple>(tuple),
+        std::make_index_sequence<rocprim::tuple_size<std::remove_reference_t<Tuple>>::value>{});
 }
 
 END_ROCPRIM_NAMESPACE
