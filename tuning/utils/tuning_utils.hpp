32ed0c075c71b9a2cfcc5ab34f60caae899128ba
Stanley Tsang <stanley.tsang@amd.com>
2025-12-09 10:21:28 -0700

    Adding missing copyrights to rocPRIM and rocThrust files (#2991)
    ## Motivation
    
    There are missing copyrights on some files for rocPRIM and Thrust.
    
    ## Technical Details
    
    Adding the appropriate copyright
    
    ## Test Plan
    
    <!-- Explain any relevant testing done to verify this PR. -->
    
    ## Test Result
    
    <!-- Briefly summarize test outcomes. -->
    
    ## Submission Checklist
    
    - [ ] Look over the contributing guidelines at
    https://github.com/ROCm/ROCm/blob/develop/CONTRIBUTING.md#pull-requests.
    
    ---------
    
    Co-authored-by: Jobbins <john.robbins@amd.com>

--- tuning/utils/tuning_utils.hpp
+++ tuning/utils/tuning_utils.hpp
@@ -1,3 +1,6 @@
+// Copyright © Advanced Micro Devices, Inc., or its affiliates.
+// SPDX-License-Identifier:  MIT
+
 #include <hip/hip_runtime.h>
 
 #define HIP_CHECK(condition)                                                                \
